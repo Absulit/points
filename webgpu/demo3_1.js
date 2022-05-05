2e5546e3f7b1a5759aa4f4e469d34c067dbe90c7
Sebastian Sanabria <sebastian@absulit.com>
2022-05-04 19:35:39 -0600

    Colored vertices

---
+++ webgpu/demo3_1.js
@@ -0,0 +1,109 @@
+'use strict';
+// import triangleVertWGSL from './shaders/triangle.vert.wgsl';
+// import redFragWGSL from './shaders/red.frag.wgsl';
+import { getShaderSource } from './shader_loader.js';
+
+const triangleVertWGSL = await getShaderSource('./shaders/triangle3_1.vert.wgsl');
+const redFragWGSL = await getShaderSource('./shaders/red3_1.frag.wgsl');
+
+const canvas = document.getElementById('gl-canvas');
+let device = null
+let context = null
+let pipeline = null
+
+/***************/
+const stats = new Stats();
+stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+document.body.appendChild(stats.dom);
+/***************/
+
+
+async function init() {
+
+    const adapter = await navigator.gpu.requestAdapter();
+    if (!adapter) { return; }
+    device = await adapter.requestDevice();
+
+    if (canvas === null) return;
+    context = canvas.getContext('webgpu');
+
+    const devicePixelRatio = window.devicePixelRatio || 1;
+    const presentationSize = [
+        canvas.clientWidth * devicePixelRatio,
+        canvas.clientHeight * devicePixelRatio,
+    ];
+    const presentationFormat = context.getPreferredFormat(adapter);
+
+    context.configure({
+        device,
+        format: presentationFormat,
+        size: presentationSize,
+        compositingAlphaMode: "opaque",
+    });
+
+    pipeline = device.createRenderPipeline({
+        vertex: {
+            module: device.createShaderModule({
+                code: triangleVertWGSL,
+            }),
+            entryPoint: 'main',
+        },
+        fragment: {
+            module: device.createShaderModule({
+                code: redFragWGSL,
+            }),
+            entryPoint: 'main',
+            targets: [
+                {
+                    format: presentationFormat,
+                },
+            ],
+        },
+        primitive: {
+            topology: 'triangle-list',
+        },
+    });
+
+    update();
+}
+
+function update() {
+    stats.begin();
+
+    // Sample is no longer the active page.
+    if (!canvas) return;
+
+    const commandEncoder = device.createCommandEncoder();
+    const textureView = context.getCurrentTexture().createView();
+
+    //const renderPassDescriptor: GPURenderPassDescriptor = {
+    const renderPassDescriptor = {
+        colorAttachments: [
+            {
+                view: textureView,
+                clearValue: { r: 0.0, g: 0.0, b: 0.0, a: 1.0 },
+                loadOp: 'clear',
+                storeOp: 'store',
+            },
+        ],
+    };
+
+    const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
+    passEncoder.setPipeline(pipeline);
+    /**
+     * vertexCount: number The number of vertices to draw
+     * instanceCount?: number | undefined The number of instances to draw
+     * firstVertex?: number | undefined Offset into the vertex buffers, in vertices, to begin drawing from
+     * firstInstance?: number | undefined First instance to draw
+     */
+    //passEncoder.draw(3, 1, 0, 0);
+    passEncoder.draw(3);
+    passEncoder.end();
+
+    device.queue.submit([commandEncoder.finish()]);
+
+    stats.end();
+    requestAnimationFrame(update);
+}
+
+init();
---
+++ webgpu/shaders/triangle3_1.vert.wgsl
@@ -0,0 +1,27 @@
+struct Fragment {
+    @builtin(position) Position: vec4<f32>,
+    @location(0) Color: vec4<f32>
+}
+
+
+@stage(vertex)
+fn main(@builtin(vertex_index) VertexIndex: u32) -> Fragment {
+    var positions = array<vec2<f32>, 3>(
+        vec2<f32>(0.0, 0.5),
+        vec2<f32>(-0.5, -0.5),
+        vec2<f32>(0.5, -0.5)
+    );
+
+    var colors = array<vec3<f32>, 3>(
+        vec3<f32>(1.0, 0.0, 0.0),
+        vec3<f32>(0.0, 1.0, 0.0),
+        vec3<f32>(0.0, 0.0, 1.0)
+    );
+
+    var output: Fragment;
+
+    output.Position = vec4<f32>(positions[VertexIndex], 0.0, 1.0);
+    output.Color = vec4<f32>(colors[VertexIndex], 1.0);
+
+    return output;
+}
