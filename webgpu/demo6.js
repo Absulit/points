409fe99ca2af54d04f114ec68788afa058dc3e6c
Sebastian Sanabria <sebastian@absulit.com>
2022-05-08 15:27:17 -0600

    Moved code to WebGPU class

--- webgpu/demo6.js
+++ webgpu/demo6.js
@@ -23,8 +23,6 @@ const shaders = {
 }
 
 
-let device = null;
-let context = null;
 let pipeline = null;
 let verticesBuffer = null;
 let uniformBindGroup = null;
@@ -52,221 +50,22 @@ const vertexArray = new Float32Array([
     -1, +1, 0, 1, 1, 1, 0, 1, 0, 0,
 ]);
 
-const vertexSize = vertexArray.BYTES_PER_ELEMENT * 10; // Byte size of ONE triangle data (vertex, color, uv). (one row)
-const positionOffset = vertexArray.BYTES_PER_ELEMENT * 0;
-const colorOffset = vertexArray.BYTES_PER_ELEMENT * 4; // Byte offset of triangle vertex color attribute.
-const vertexCount = vertexArray.byteLength / vertexSize;
-const UVOffset = vertexArray.BYTES_PER_ELEMENT * 8;
-
-
-
-// const w = 1;
-// const h = 1;
-// const vertexArray = new Float32Array([
-//     -w, -h, 0, 1, 1, 0, 0, 1,
-//     -w, +h, 0, 0, 1, 0, 0, 1,
-//     +w, -h, 1, 1, 1, 0, 0, 1,
-//     +w, +h, 1, 0, 1, 0, 0, 1,
-// ]);
-
-
-// const vertexSize = vertexArray.BYTES_PER_ELEMENT * 8; // Byte size of ONE triangle data (vertex, color, uv). (one row)
-// const positionOffset = vertexArray.BYTES_PER_ELEMENT * 0;
-// const colorOffset = vertexArray.BYTES_PER_ELEMENT * 4; // Byte offset of triangle vertex color attribute.
-// const vertexCount = vertexArray.byteLength / vertexSize;
-// const UVOffset = vertexArray.BYTES_PER_ELEMENT * 2;
-
-
-console.log('vertexArray.BYTES_PER_ELEMENT:', vertexArray.BYTES_PER_ELEMENT);
-console.log('vertexArray.byteLength:', vertexArray.byteLength);
-console.log('vertexCount  = vertexArray.byteLength / vertexSize:', vertexArray.byteLength / vertexSize);
-
-console.log({ vertexSize, positionOffset, colorOffset, vertexCount, UVOffset });
 
 const webGPU = new WebGPU('gl-canvas');
-
+webGPU.useTexture = true;
 async function init() {
-
-    //initWebGPU('gl-canvas');
-    await webGPU.init();
-
-    // Create a vertex buffer from the triangle data.
-    // verticesBuffer = device.createBuffer({
-    //     size: vertexArray.byteLength,
-    //     usage: GPUBufferUsage.VERTEX,
-    //     mappedAtCreation: true,
-    // });
-    // new Float32Array(verticesBuffer.getMappedRange()).set(vertexArray);
-    // verticesBuffer.unmap();
-    verticesBuffer = webGPU.createVertexBuffer(vertexArray);
-
-
-    // enum GPUPrimitiveTopology {
-    //     "point-list",
-    //     "line-list",
-    //     "line-strip",
-    //     "triangle-list",
-    //     "triangle-strip",
-    // };
-
-    pipeline = device.createRenderPipeline({
-        layout: 'auto',
-        primitive: { topology: 'triangle-strip' },
-        vertex: {
-            module: device.createShaderModule({
-                code: shaders[useTexture].vertex,
-            }),
-            entryPoint: 'main', // shader function name
-
-            buffers: [
-                {
-                    arrayStride: vertexSize,
-                    //arrayStride: vertexArray.BYTES_PER_ELEMENT,
-                    //arrayStride: vertexCount * vertexArray.BYTES_PER_ELEMENT,//vertexSize,
-                    attributes: [
-                        {
-                            // position
-                            shaderLocation: 0,
-                            offset: positionOffset,
-                            format: 'float32x4',
-                        },
-                        {
-                            // colors
-                            shaderLocation: 1,
-                            offset: colorOffset,
-                            format: 'float32x4',
-                        },
-                        {
-                            // uv
-                            shaderLocation: 2,
-                            offset: UVOffset,
-                            format: 'float32x2',
-                        },
-                    ],
-                },
-            ],
-        },
-        fragment: {
-            module: device.createShaderModule({
-                code: shaders[useTexture].fragment,
-            }),
-            entryPoint: 'main', // shader function name
-            targets: [
-                {
-                    format: presentationFormat,
-
-                    blend: {
-                        alpha: {
-                            srcFactor: 'src-alpha',
-                            dstFactor: 'one-minus-src-alpha',
-                            operation: 'add'
-                        },
-                        color: {
-                            srcFactor: 'src-alpha',
-                            dstFactor: 'one-minus-src-alpha',
-                            operation: 'add'
-                        },
-                    },
-                    writeMask: GPUColorWrite.ALL,
-
-                },
-            ],
-        },
-        primitive: {
-            topology: 'triangle-list',
-        },
-    });
-
-    // -- create texture
-
-
-    if (useTexture) {
-        const samp = device.createSampler({ minFilter: "linear", magFilter: "linear" });
-        // Fetch the image and upload it into a GPUTexture.
-        //let cubeTexture: GPUTexture;
-        let cubeTexture;
-        {
-            const response = await fetch('./assets/old_king_600x600.jpg');
-            const blob = await response.blob();
-            const imageBitmap = await createImageBitmap(blob);
-
-            cubeTexture = device.createTexture({
-                size: [imageBitmap.width, imageBitmap.height, 1],
-                format: 'rgba8unorm',
-                usage:
-                    GPUTextureUsage.TEXTURE_BINDING |
-                    GPUTextureUsage.COPY_DST |
-                    GPUTextureUsage.RENDER_ATTACHMENT,
-            });
-            device.queue.copyExternalImageToTexture(
-                { source: imageBitmap },
-                { texture: cubeTexture },
-                [imageBitmap.width, imageBitmap.height]
-            );
-        }
-
-        console.log(cubeTexture);
-
-        uniformBindGroup = device.createBindGroup({
-            layout: pipeline.getBindGroupLayout(0),
-            entries: [
-                {
-                    binding: 0,
-                    resource: cubeTexture.createView(),
-                },
-                {
-                    binding: 1,
-                    resource: samp,
-                }
-            ],
-        });
+    const initialized = await webGPU.init();
+    if (initialized) {
+        webGPU.createVertexBuffer(vertexArray);
+        await webGPU.createPipeline();
     }
-
-
-    // create texture --
-
     update();
 }
 
 function update() {
     stats.begin();
 
-    // Sample is no longer the active page.
-    if (!canvas) return;
-
-    const commandEncoder = device.createCommandEncoder();
-    const textureView = context.getCurrentTexture().createView();
-
-    //const renderPassDescriptor: GPURenderPassDescriptor = {
-    const renderPassDescriptor = {
-        colorAttachments: [
-            {
-                view: textureView,
-                clearValue: { r: 0.0, g: 0.0, b: 0.0, a: 1.0 },
-                loadOp: 'clear',
-                storeOp: 'store',
-            },
-        ],
-    };
-
-    const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
-    passEncoder.setPipeline(pipeline);
-    if (useTexture) {
-        passEncoder.setBindGroup(0, uniformBindGroup);
-    }
-    passEncoder.setVertexBuffer(0, verticesBuffer);
-
-    /**
-     * vertexCount: number The number of vertices to draw
-     * instanceCount?: number | undefined The number of instances to draw
-     * firstVertex?: number | undefined Offset into the vertex buffers, in vertices, to begin drawing from
-     * firstInstance?: number | undefined First instance to draw
-     */
-    //passEncoder.draw(3, 1, 0, 0);
-    passEncoder.draw(vertexCount);
-    passEncoder.end();
-
-    device.queue.submit([commandEncoder.finish()]);
+    webGPU.update();
 
     stats.end();
     requestAnimationFrame(update);
--- webgpu/js/absulit.webgpu.module.js
+++ webgpu/js/absulit.webgpu.module.js
@@ -1,17 +1,88 @@
+import { getShaderSource } from '../shader_loader.js';
+
+export class VertexBufferInfo {
+    /**
+     * Along with the vertexArray it calculates some info like offsets required for the pipeline.
+     * @param {Float32Array} vertexArray array with vertex, color and uv data
+     * @param {Number} triangleDataLength how many items does a triangle row has in vertexArray
+     * @param {Number} vertexOffset index where the vertex data starts in a row of `triangleDataLength` items
+     * @param {Number} colorOffset index where the color data starts in a row of `triangleDataLength` items
+     * @param {Number} uvOffset index where the uv data starts in a row of `triangleDataLength` items
+     */
+    constructor(vertexArray, triangleDataLength = 10, vertexOffset = 0, colorOffset = 4, uvOffset = 8) {
+        this._vertexSize = vertexArray.BYTES_PER_ELEMENT * triangleDataLength; // Byte size of ONE triangle data (vertex, color, uv). (one row)
+        this._vertexOffset = vertexArray.BYTES_PER_ELEMENT * vertexOffset;
+        this._colorOffset = vertexArray.BYTES_PER_ELEMENT * colorOffset; // Byte offset of triangle vertex color attribute.
+        this._uvOffset = vertexArray.BYTES_PER_ELEMENT * uvOffset;
+        this._vertexCount = vertexArray.byteLength / this._vertexSize;
+
+        console.log('vertexArray.BYTES_PER_ELEMENT:', vertexArray.BYTES_PER_ELEMENT);
+        console.log('vertexArray.byteLength:', vertexArray.byteLength);
+        console.log('vertexCount  = vertexArray.byteLength / vertexSize:', vertexArray.byteLength / this._vertexSize);
+
+        console.log({ vertexSize: this._vertexSize, vertexOffset: this._vertexOffset, colorOffset: this._colorOffset, vertexCount: this._vertexCount, uvOffset: this._uvOffset });
+    }
+
+    get vertexSize() {
+        return this._vertexSize
+    }
+
+    get vertexOffset() {
+        return this._vertexOffset;
+    }
+
+    get colorOffset() {
+        return this._colorOffset;
+    }
+
+    get uvOffset() {
+        return this._uvOffset;
+    }
+
+    get vertexCount() {
+        return this._vertexCount;
+    }
+}
+
 export default class WebGPU {
     constructor(canvasId) {
         this._canvasId = canvasId;
         this._canvas = null;
         this._device = null;
         this._context = null;
+        this._presentationFormat = null;
+        this._useTexture = false;
+        this._shaders = null;
+        this._pipeline = null;
+        this._vertexBufferInfo = null;
+        this._buffer = null;
+        this._uniformBindGroup = null;
+
     }
 
     async init() {
+        const colorsVertWGSL = await getShaderSource('./shaders/demo6_colors.vert.wgsl');
+        const colorsFragWGSL = await getShaderSource('./shaders/demo6_colors.frag.wgsl');
+
+        const textureVertWGSL = await getShaderSource('./shaders/demo6_texture.vert.wgsl');
+        const textureFragWGSL = await getShaderSource('./shaders/demo6_texture.frag.wgsl');
+
+        this._shaders = {
+            false: {
+                vertex: colorsVertWGSL,
+                fragment: colorsFragWGSL
+            },
+            true: {
+                vertex: textureVertWGSL,
+                fragment: textureFragWGSL
+            }
+        }
+
         this._canvas = document.getElementById(this._canvasId);
         const adapter = await navigator.gpu.requestAdapter();
         if (!adapter) { return false; }
-        this._device  = await adapter.requestDevice();
-        this._device .lost.then(info => {
+        this._device = await adapter.requestDevice();
+        this._device.lost.then(info => {
             console.log(info);
         });
 
@@ -23,11 +94,11 @@ export default class WebGPU {
             this._canvas.clientWidth * devicePixelRatio,
             this._canvas.clientHeight * devicePixelRatio,
         ];
-        const presentationFormat = this._context.getPreferredFormat(adapter);
-
+        this._presentationFormat = this._context.getPreferredFormat(adapter);
+        console.log({ _presentationFormat: this._presentationFormat });
         this._context.configure({
             device: this._device,
-            format: presentationFormat,
+            format: this._presentationFormat,
             size: presentationSize,
             compositingAlphaMode: 'premultiplied',
         });
@@ -36,19 +107,180 @@ export default class WebGPU {
     }
 
     /**
-     * 
-     * @param {Float32Array} vertexArray 
+     *
+     * @param {Float32Array} vertexArray
      * @returns buffer
      */
-    createVertexBuffer(vertexArray){
-        const buffer = this._device.createBuffer({
+    createVertexBuffer(vertexArray) {
+        this._vertexBufferInfo = new VertexBufferInfo(vertexArray);
+
+        this._buffer = this._device.createBuffer({
             size: vertexArray.byteLength,
             usage: GPUBufferUsage.VERTEX,
             mappedAtCreation: true,
         });
-        new Float32Array(buffer.getMappedRange()).set(vertexArray);
-        buffer.unmap();
-        return buffer
+        new Float32Array(this._buffer.getMappedRange()).set(vertexArray);
+        this._buffer.unmap();
+        return this._buffer;
+    }
+
+    async createPipeline() {
+        // enum GPUPrimitiveTopology {
+        //     'point-list',
+        //     'line-list',
+        //     'line-strip',
+        //     'triangle-list',
+        //     'triangle-strip',
+        // };
+        console.log({ _vertexBufferInfo: this._vertexBufferInfo });
+        this._pipeline = this._device.createRenderPipeline({
+            layout: 'auto',
+            primitive: { topology: 'triangle-strip' },
+            primitive: { topology: 'triangle-list' },
+            vertex: {
+                module: this._device.createShaderModule({
+                    code: this._shaders[this._useTexture].vertex,
+                }),
+                entryPoint: 'main', // shader function name
+
+                buffers: [
+                    {
+                        arrayStride: this._vertexBufferInfo.vertexSize,
+                        attributes: [
+                            {
+                                // position
+                                shaderLocation: 0,
+                                offset: this._vertexBufferInfo.vertexOffset,
+                                format: 'float32x4',
+                            },
+                            {
+                                // colors
+                                shaderLocation: 1,
+                                offset: this._vertexBufferInfo.colorOffset,
+                                format: 'float32x4',
+                            },
+                            {
+                                // uv
+                                shaderLocation: 2,
+                                offset: this._vertexBufferInfo.uvOffset,
+                                format: 'float32x2',
+                            },
+                        ],
+                    },
+                ],
+            },
+            fragment: {
+                module: this._device.createShaderModule({
+                    code: this._shaders[this._useTexture].fragment,
+                }),
+                entryPoint: 'main', // shader function name
+                targets: [
+                    {
+                        format: this._presentationFormat,
+
+                        blend: {
+                            alpha: {
+                                srcFactor: 'src-alpha',
+                                dstFactor: 'one-minus-src-alpha',
+                                operation: 'add'
+                            },
+                            color: {
+                                srcFactor: 'src-alpha',
+                                dstFactor: 'one-minus-src-alpha',
+                                operation: 'add'
+                            },
+                        },
+                        writeMask: GPUColorWrite.ALL,
+
+                    },
+                ],
+            },
+
+        });
+        if (this._useTexture) {
+            await this._createTexture();
+        }
+    }
+
+    async _createTexture() {
+        const samp = this._device.createSampler({ minFilter: 'linear', magFilter: 'linear' });
+        // Fetch the image and upload it into a GPUTexture.
+        //let cubeTexture: GPUTexture;
+        let cubeTexture;
+        {
+            const response = await fetch('./assets/old_king_600x600.jpg');
+            const blob = await response.blob();
+            const imageBitmap = await createImageBitmap(blob);
+
+            cubeTexture = this._device.createTexture({
+                size: [imageBitmap.width, imageBitmap.height, 1],
+                format: 'rgba8unorm',
+                usage:
+                    GPUTextureUsage.TEXTURE_BINDING |
+                    GPUTextureUsage.COPY_DST |
+                    GPUTextureUsage.RENDER_ATTACHMENT,
+            });
+            this._device.queue.copyExternalImageToTexture(
+                { source: imageBitmap },
+                { texture: cubeTexture },
+                [imageBitmap.width, imageBitmap.height]
+            );
+        }
+
+        console.log(cubeTexture);
+
+        this._uniformBindGroup = this._device.createBindGroup({
+            layout: this._pipeline.getBindGroupLayout(0),
+            entries: [
+                {
+                    binding: 0,
+                    resource: cubeTexture.createView(),
+                },
+                {
+                    binding: 1,
+                    resource: samp,
+                }
+            ],
+        });
+    }
+
+    update() {
+        if (!this._canvas) return;
+
+        const commandEncoder = this._device.createCommandEncoder();
+        const textureView = this._context.getCurrentTexture().createView();
+
+        //const renderPassDescriptor: GPURenderPassDescriptor = {
+        const renderPassDescriptor = {
+            colorAttachments: [
+                {
+                    view: textureView,
+                    clearValue: { r: 0.0, g: 0.0, b: 0.0, a: 1.0 },
+                    loadOp: 'clear',
+                    storeOp: 'store',
+                },
+            ],
+        };
+
+        const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
+        passEncoder.setPipeline(this._pipeline);
+        if (this._useTexture) {
+            passEncoder.setBindGroup(0, this._uniformBindGroup);
+        }
+        passEncoder.setVertexBuffer(0, this._buffer);
+
+        /**
+         * vertexCount: number The number of vertices to draw
+         * instanceCount?: number | undefined The number of instances to draw
+         * firstVertex?: number | undefined Offset into the vertex buffers, in vertices, to begin drawing from
+         * firstInstance?: number | undefined First instance to draw
+         */
+        //passEncoder.draw(3, 1, 0, 0);
+        passEncoder.draw(this._vertexBufferInfo.vertexCount);
+        passEncoder.end();
+
+        this._device.queue.submit([commandEncoder.finish()]);
+
     }
 
     get canvas() {
@@ -59,7 +291,26 @@ export default class WebGPU {
         return this._device;
     }
 
-    get context(){
+    get context() {
         return this._context;
+    }
+
+    get presentationFormat() {
+        return this._presentationFormat;
+    }
+
+    /**
+     * @param {Boolean} value
+     */
+    set useTexture(value) {
+        this._useTexture = value;
+    }
+
+    get useTexture() {
+        return this._useTexture;
+    }
+
+    get pipeline() {
+        return this._pipeline;
     }
 }
