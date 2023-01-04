271464cde032e63ab767febb736bbbc67972ab9b
Sebastian Sanabria <sebastian@absulit.com>
2023-01-03 20:16:40 -0600

    distorting image with lines

--- simplewebgpu/shaders/imagetexture3/imagetexture3.frag.js
+++ simplewebgpu/shaders/imagetexture3/imagetexture3.frag.js
@@ -25,21 +25,23 @@ fn main(
         @builtin(position) position: vec4<f32>
     ) -> @location(0) vec4<f32> {
 
-    let n1 = snoise(uv * 10);
+    let n1 = snoise(uv + 2 * fnusin(1));
 
     let dims: vec2<u32> = textureDimensions(image, 0);
     var dimsRatio = f32(dims.x) / f32(dims.y);
 
     let imageUV = uv * vec2(1,-1 * dimsRatio) * ratio.y / params.sliderA;
     //let oldKingUVClamp = uv * vec2(1,1 * dimsRatio) * ratio.x;
-    let rgbaImage = textureSample(image, feedbackSampler, imageUV); //* .998046;
-
+    let lines = sin( uv.x*(uv.x + 3 * fnusin(1))  ) ;
+    let rgbaImage = textureSample(image, feedbackSampler, imageUV * lines); //* .998046;
+    
     let b = brightness(rgbaImage);
     let d = distance(uv, rgbaImage.xy);
 
-    //let finalColor:vec4<f32> = vec4(b);
-    let finalColor:vec4<f32> = mix(  vec4(1,0,0,1)   , vec4(1,1,0,1) , b) * fnusin(d);
+    //let finalColor:vec4<f32> = mix(  vec4(1,0,0,1)   , vec4(1,1,0,1) , b) * fnusin(d);
     //let finalColor:vec4<f32> = rgbaImage;
+    //let finalColor:vec4<f32> = vec4(b);
+    let finalColor:vec4<f32> = vec4( b  );
 
 
     return finalColor;
