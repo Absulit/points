4cb09d0a67f74f8fc03efb79791832e9806c5ef1
Sebastian Sanabria <sebastian@absulit.com>
2023-02-03 21:52:15 -0600

    dynamically loading the examples

--- examples/main.js
+++ examples/main.js
@@ -1,31 +1,6 @@
 'use strict';
 import * as dat from './../src/vendor/datgui/dat.gui.module.js';
-import { print } from '../legacy/js/utils.js';
 import Points, { ShaderType } from '../src/absulit.points.module.js';
-import base from '../src/shaders/base/index.js';
-import bloom1 from './bloom1/index.js';
-import circleblur from './circleblur/index.js';
-import demo6 from './demo_6/index.js';
-import dithering1 from './dithering1/index.js';
-import dithering2 from './dithering2/index.js';
-import dithering3 from './dithering3/index.js';
-import dithering4 from './dithering4/index.js';
-import imagescale1 from './imagescale1/index.js';
-import imagetexture1 from './imagetexture1/index.js';
-import imagetexture2 from './imagetexture2/index.js';
-import imagetexture3 from './imagetexture3/index.js';
-import imagetexture4 from './imagetexture4/index.js';
-import layers1 from './layers1/index.js';
-import mesh1 from './mesh1/index.js';
-import noise1 from './noise1/index.js';
-import noisecircle1 from './noisecircle1/index.js';
-import random1 from './random1/index.js';
-import random2 from './random2/index.js';
-import random3 from './random3/index.js';
-import shapes1 from './shapes1/index.js';
-import shapes2 from './shapes2/index.js';
-import videotexture1 from './videotexture1/index.js';
-
 
 /***************/
 const stats = new Stats();
@@ -38,12 +13,12 @@ let capturer = new CCapture({
     verbose: true
 });
 
-const gui = new dat.GUI({name: 'My GUI'});
+const gui = new dat.GUI({ name: 'My GUI' });
 const slidersFolder = gui.addFolder('Sliders');
 
 slidersFolder.open();
 
-const sliders = { 'a': 0, 'b': 0, 'c': 0 }
+const sliders = { 'a': 0, 'b': 0, 'c': 0 };
 
 sliders.a = Number(localStorage.getItem('slider-a')) || 0;
 sliders.b = Number(localStorage.getItem('slider-b')) || 0;
@@ -54,54 +29,77 @@ slidersFolder.add(sliders, 'b', -1, 1, .0001).name('params.sliderB').onFinishCha
 slidersFolder.add(sliders, 'c', -1, 1, .0001).name('params.sliderC').onFinishChange(() => localStorage.setItem('slider-c', sliders.c));
 
 let statsVisible = (localStorage.getItem('stats-visible') === 'true') || false;
-function setStatsVisibility(value){
+function setStatsVisibility(value) {
     value && (stats.dom.style.display = 'block');
     !value && (stats.dom.style.display = 'none');
     localStorage.setItem('stats-visible', value);
 }
 setStatsVisibility(statsVisible);
 
-let stats2 = {'visible': statsVisible};
+let stats2 = { 'visible': statsVisible };
 gui.add(stats2, 'visible').name('Show Stats').onChange(value => setStatsVisibility(value));
+
+const shaderPaths = [
+    { name: 'Base', shader: '../src/shaders/base/index.js' },
+    { name: 'Bloom1', shader: './bloom1/index.js' },
+    { name: 'Circle Blur', shader: './circleblur/index.js' },
+    { name: 'Demo 6', shader: './demo_6/index.js' },
+    { name: 'Dithering 1', shader: './dithering1/index.js' },
+    { name: 'Dithering 2', shader: './dithering2/index.js' },
+    { name: 'Dithering 3', shader: './dithering3/index.js' },
+    { name: 'Dithering 4', shader: './dithering4/index.js' },
+    { name: 'Image Scale 1', shader: './imagescale1/index.js' },
+    { name: 'Image Texture 1', shader: './imagetexture1/index.js' },
+    { name: 'Image Texture 2', shader: './imagetexture2/index.js' },
+    { name: 'Image Texture 3', shader: './imagetexture3/index.js' },
+    { name: 'Image Texture 4', shader: './imagetexture4/index.js' },
+    { name: 'Layers 1', shader: './layers1/index.js' },
+    { name: 'Mesh 1', shader: './mesh1/index.js' },
+    { name: 'Noise 1', shader: './noise1/index.js' },
+    { name: 'Noise Circle 1', shader: './noisecircle1/index.js' },
+    { name: 'Random 1', shader: './random1/index.js' },
+    { name: 'Random 2', shader: './random2/index.js' },
+    { name: 'Random 3', shader: './random3/index.js' },
+    { name: 'Shapes 1', shader: './shapes1/index.js' },
+    { name: 'Shapes 2', shader: './shapes2/index.js' },
+    { name: 'Video Texture 1', shader: './videotexture1/index.js' },
+]
+
+const shaderNames = {};
+shaderPaths.forEach((item, index) => {
+    shaderNames[item.name] = index;
+});
+
+let selectedShader = { index: 0 }
+
+let examples = gui.add(selectedShader, 'index', shaderNames).name('Examples');
+
+async function loadShaderByIndex(index) {
+    let shaderPath = shaderPaths[index].shader;
+    shaders = (await import(shaderPath)).default;
+    await init();
+}
+
+
+examples.onChange(loadShaderByIndex);
+
 /***************/
 
-
-const points = new Points('gl-canvas');
-points.useTexture = false;
-
-
+let points;
 
 let shaders;
+let animationFrameId = null;
+await loadShaderByIndex(0);
 
 async function init() {
+    if (animationFrameId) {
+        cancelAnimationFrame(animationFrameId);
+    }
+
+    points = new Points('gl-canvas');
     points.addUniform('sliderA', 0);
     points.addUniform('sliderB', 0);
     points.addUniform('sliderC', 0);
-
-    shaders = base;
-
-    // shaders = mesh1;
-    // shaders = demo6;
-    // shaders = imagescale1;
-    // shaders = bloom1;
-    // shaders = imagetexture1;
-    // shaders = imagetexture2;
-    // shaders = imagetexture3;
-    // shaders = imagetexture4;
-    // shaders = dithering1;
-    // shaders = dithering2;
-    // shaders = dithering3;
-    // shaders = dithering4;
-    // shaders = videotexture1;
-    // shaders = noise1;
-    // shaders = noisecircle1;
-    // shaders = layers1;
-    // shaders = shapes1;
-    // shaders = shapes2;
-    // shaders = random1;
-    // shaders = random2;
-    // shaders = random3;
-    // shaders = circleblur;
 
 
     await shaders.init(points);
@@ -126,10 +124,10 @@ function update() {
     stats.end();
 
     capturer.capture(points.canvas);
-    requestAnimationFrame(update);
+    animationFrameId = requestAnimationFrame(update);
 }
 
-init();
+//init();
 
 const ccaptureBtn = document.getElementById('ccaptureBtn');
 let started = false;
