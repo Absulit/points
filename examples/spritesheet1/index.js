59e3052bd6be3755eabcd8b6aab7264424df2b09
Sebastian Sanabria <sebastian@absulit.com>
2023-02-05 20:11:32 -0600

    progress on sprites
    - debugging

--- examples/spritesheet1/index.js
+++ examples/spritesheet1/index.js
@@ -12,6 +12,7 @@ const spritesheet1 = {
         //await points.addTextureImage('image', './../img/old_king_600x600.jpg', ShaderType.FRAGMENT);
         //await points.addTextureImage('image', './../assets_ignore/absulit_800x800.jpg', ShaderType.FRAGMENT);
         await points.addTextureImage('image', './../img/sprite_nums_640x640.png', ShaderType.FRAGMENT);
+        // await points.addTextureImage('image', './../assets_ignore/pixelspritefont 32_green.png', ShaderType.FRAGMENT);
     },
     update: (points) => {
 
