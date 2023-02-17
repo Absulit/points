d4f01f8f1b211a6aa1f3116c6a4b42de22368ae5
Sebastian Sanabria <sebastian@absulit.com>
2023-02-16 20:16:52 -0600

    mouse events

---
+++ examples/mouseclickscroll1/index.js
@@ -0,0 +1,18 @@
+import vert from './vert.js';
+import compute from './compute.js';
+import frag from './frag.js';
+import { ShaderType } from '../../src/absulit.points.module.js';
+
+const mouseclickscroll1 = {
+    vert,
+    compute,
+    frag,
+    init: async points => {
+        points.addStorage('variables', 1, 'Variable', 4, ShaderType.FRAGMENT);
+    },
+    update: points => {
+
+    }
+}
+
+export default mouseclickscroll1;
