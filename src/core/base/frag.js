1f1939fb01fae4094c13f2e5cf32dc0bfb201ca7
Sebastian Sanabria <sebastian@absulit.com>
2023-02-18 16:39:03 -0600

    small changes in base example

--- src/core/base/frag.js
+++ src/core/base/frag.js
@@ -6,13 +6,13 @@ ${fnusin}
 
 @fragment
 fn main(
-        @location(0) color: vec4<f32>,
-        @location(1) uv: vec2<f32>,
-        @location(2) ratio: vec2<f32>,  // relation between params.screenWidth and params.screenHeight
-        @location(3) uvr: vec2<f32>,    // uv with aspect ratio corrected
-        @location(4) mouse: vec2<f32>,
-        @builtin(position) position: vec4<f32>
-    ) -> @location(0) vec4<f32> {
+    @location(0) color: vec4<f32>,
+    @location(1) uv: vec2<f32>,
+    @location(2) ratio: vec2<f32>,  // relation between params.screenWidth and params.screenHeight
+    @location(3) uvr: vec2<f32>,    // uv with aspect ratio corrected
+    @location(4) mouse: vec2<f32>,
+    @builtin(position) position: vec4<f32>
+) -> @location(0) vec4<f32> {
 
     let cellSize = 20. + 10. * fnusin(1.);
     let a = sin(uvr.x  * cellSize) * sin(uvr.y * cellSize);
