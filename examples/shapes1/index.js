c6c724ff8801fa17f75f3c871e8ad87a228d5379
Sebastian Sanabria <sebastian@absulit.com>
2023-06-03 15:13:21 -0600

    shapes 1 with better workgroups

--- examples/shapes1/index.js
+++ examples/shapes1/index.js
@@ -1,10 +1,11 @@
 import vert from './vert.js';
 import compute from './compute.js';
 import frag from './frag.js';
+import { RenderPass } from '../../src/absulit.points.module.js';
 const shapes1 = {
-    vert,
-    compute,
-    frag,
+    renderPasses: [
+        new RenderPass(vert, frag, compute, 128, 1, 1)
+    ],
     init: async points => {
         const numPoints = 128;
         points.addUniform('numPoints', numPoints);
