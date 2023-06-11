9fe269670f330d4c1640957a3db0f884b7b95881
Sebastian Sanabria <sebastian@absulit.com>
2023-06-10 19:01:25 -0600

    Simplified circleblur compute
    - with better workgroups

--- examples/circleblur/index.js
+++ examples/circleblur/index.js
@@ -1,10 +1,12 @@
 import vert from './vert.js';
 import compute from './compute.js';
 import frag from './frag.js';
+import { RenderPass } from '../../src/absulit.points.module.js';
 const circleblur = {
-    vert,
-    compute,
-    frag,
+    renderPasses: [
+        new RenderPass(null, null, compute, 800, 800, 1),
+        new RenderPass(vert, frag, null, 8, 1, 1)
+    ],
     init: async points => {
         points.addSampler('feedbackSampler');
         points.addTexture2d('feedbackTexture', true);
