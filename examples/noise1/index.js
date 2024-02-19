4cc14fe1edc6c38bc89f6a72f14604f3fa2d5aba
Sebastian Sanabria <sebastian@absulit.com>
2024-02-18 19:24:55 -0600

    noise 1 datgui folder

--- examples/noise1/index.js
+++ examples/noise1/index.js
@@ -1,16 +1,34 @@
 import vert from './vert.js';
 import frag from './frag.js';
+
+const options = {
+    scale0: .291,
+    scale1: .018,
+    scale2: .059,
+}
+
 const noise1 = {
     vert,
     frag,
-    init: async points => {
+    init: async (points, folder) => {
         const numPoints = 800*800;
         points.addUniform('value_noise_data_length', numPoints);
         points.addStorage('value_noise_data', `array<f32, ${numPoints}>`);
         points.addStorage('variables', 'Variable');
+
+        points.addUniform('scale0', options.scale0, 'f32');
+        points.addUniform('scale1', options.scale1, 'f32');
+        points.addUniform('scale2', options.scale2, 'f32');
+
+        folder.add(options, 'scale0', 0, 1, .0001).name('Scale 0');
+        folder.add(options, 'scale1', 0, 1, .0001).name('Scale 1');
+        folder.add(options, 'scale2', 0, 1, .0001).name('Scale 2');
+        folder.open();
     },
     update: points => {
-
+        points.updateUniform('scale0', options.scale0);
+        points.updateUniform('scale1', options.scale1);
+        points.updateUniform('scale2', options.scale2);
     }
 }
 
