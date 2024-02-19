d97446244969f45bcff4363574a2379b7cbcc080
Sebastian Sanabria <sebastian@absulit.com>
2024-02-19 14:36:46 -0600

    video texture 1 datgui folder

--- examples/videotexture1/index.js
+++ examples/videotexture1/index.js
@@ -4,17 +4,26 @@
  * https://www.pexels.com/video/big-beak-bird-on-a-tree-branch-4746616/
  */
 
+const options = {
+    scale: .180,
+}
+
 import vert from './vert.js';
 import frag from './frag.js';
 const videotexture1 = {
     vert,
     frag,
-    init: async points => {
+    init: async (points, folder) => {
         points.addSampler('feedbackSampler', null);
-        await points.addTextureVideo('video', './../img/pexels-shubh-haque-4746616-960x540-30fps.mp4')
+        await points.addTextureVideo('video', './../img/pexels-shubh-haque-4746616-960x540-30fps.mp4');
+
+        points.addUniform('scale', options.scale, 'f32');
+
+        folder.add(options, 'scale', 0, 1, .0001).name('Scale');
+        folder.open();
     },
     update: points => {
-
+        points.updateUniform('scale', options.scale);
     }
 }
 
