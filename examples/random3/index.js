29c8f82a04662806215be30253e95898ed395eff
Sebastian Sanabria <sebastian@absulit.com>
2024-02-19 11:50:28 -0600

    random 3 datgui folder

--- examples/random3/index.js
+++ examples/random3/index.js
@@ -3,18 +3,26 @@ import compute from './compute.js';
 import frag from './frag.js';
 import RenderPass from 'renderpass';
 
+const options = {
+    sliderA: .291,
+}
+
 const random3 = {
 
     renderPasses: [
         new RenderPass(vert, frag, compute, 800, 800)
     ],
-    init: async points => {
+    init: async (points, folder) => {
         points.addSampler('feedbackSampler');
         points.addTexture2d('feedbackTexture', true);
         points.addBindingTexture('outputTex', 'computeTexture');
+
+        points.addUniform('sliderA', options.sliderA, 'f32');
+        folder.add(options, 'sliderA', 0, 1, .0001).name('sliderA');
+        folder.open();
     },
     update: points => {
-
+        points.updateUniform('sliderA', options.sliderA);
     }
 }
 
