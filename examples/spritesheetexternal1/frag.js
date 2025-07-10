7c3bdbeb462e8b4c12c7302ff4c92bbae28af3ee
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-07-10 14:42:07 -0600

    scaffold

---
+++ examples/spritesheetexternal1/frag.js
@@ -0,0 +1,29 @@
+import { fnusin } from 'animation';
+
+const frag = /*wgsl*/`
+
+${fnusin}
+
+@fragment
+fn main(
+    @location(0) color: vec4<f32>,
+    @location(1) uv: vec2<f32>,
+    @location(2) ratio: vec2<f32>,  // relation between params.screen.x and params.screen.y
+    @location(3) uvr: vec2<f32>,    // uv with aspect ratio corrected
+    @location(4) mouse: vec2<f32>,
+    @builtin(position) position: vec4<f32>
+) -> @location(0) vec4<f32> {
+
+    let cellSize = 20. + 10. * fnusin(1.);
+    let a = sin(uvr.x  * cellSize) * sin(uvr.y * cellSize);
+    let b = sin(uvr.x * uvr.y * 10. * 9.1 * .25 );
+    let c = fnusin(uvr.x * uvr.y * 10.);
+    let d = distance(a,b);
+    let f = d * uvr.x * uvr.y;
+    let finalColor:vec4<f32> = vec4(a*d,f*c*a,f, 1.);
+
+    return finalColor;
+}
+`;
+
+export default frag;
