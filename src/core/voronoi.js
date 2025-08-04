2231b123fa3116bb6d05f59f1197091cfd754036
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-08-03 19:56:49 -0600

    docs updated

--- src/core/voronoi.js
+++ src/core/voronoi.js
@@ -1,7 +1,11 @@
 /**
+ * Voronoi functions.
+ *
  * These are wgsl functions, not js functions.
  * The function is enclosed in a js string constant,
  * to be appended into the code to reference it in the string shader.
+ *
+ * @author Sebastián Sanabria Díaz sebastian@absulit.com
  * @module points/voronoi
  */
 
@@ -10,11 +14,19 @@ import { rand } from './random.js';
 // Created by Sebastián Sanabria Díaz absulit.com
 
 /**
- * Voronoi noise
+ * Voronoi noise. Generated randomly.
  * @type {String}
  * @param {vec2f} p origin point
  * @param {u32} numPoints number of cells
- * @return `vec2f`
+ * @return {vec2f}
+ *
+ * @example
+ * // js
+ * import { voronoi } from 'points/voronoi';
+ *
+ * // wgsl string
+ * ${voronoi}
+ * let value = voronoi(uvr, numPoints);
  */
 export const voronoi = /*wgsl*/`
 
