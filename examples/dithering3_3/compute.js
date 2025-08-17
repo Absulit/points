bb457d46f774029ed5b3e5272364d406df1986f4
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-08-17 12:40:27 -0600

    storing values

--- examples/dithering3_3/compute.js
+++ examples/dithering3_3/compute.js
@@ -8,6 +8,8 @@ struct Variable{
 
 ${brightness}
 
+const distance = 1;
+
 fn newB(point:vec4f) -> f32 {
     let b = brightness(point);
     return step(.5, b); // if(b > .5){newBrightness = 1.;}
@@ -37,13 +39,11 @@ fn main(
     let newBrightness = step(.5, b); // if(b > .5){newBrightness = 1.;}
 
     let quant_error = b - newBrightness;
-    let distance = 1;
-    let distanceU = u32(distance);
     point = vec4(newBrightness);
     textureStore(outputTex, GlobalId.xy, point);
 
 
-    let rightPosition = GlobalId.xy + vec2(distanceU, 0);
+    let rightPosition = GlobalId.xy + vec2(distance, 0);
     var rightPoint = textureLoad(image, rightPosition, 0);
     let right_new_brightness = newB(rightPoint);
     // rightPoint = vec4(brightness(rightPoint) + (.5 * quant_error * params.quantError));
@@ -51,11 +51,11 @@ fn main(
     textureStore(outputTex, rightPosition, rightPoint);
 
 
-    let bottomPosition = GlobalId.xy + vec2(0, distanceU);
+    let bottomPosition = GlobalId.xy + vec2(0, distance);
     var bottomPoint = textureLoad(image, bottomPosition, 0);
     let bottom_new_brightness = newB(bottomPoint);
-    bottomPoint = vec4(brightness(bottomPoint) + (.5 * quant_error));
-    bottomPoint = vec4(bottom_new_brightness + (.5 * quant_error));
+    // bottomPoint = vec4(brightness(bottomPoint) + (.5 * quant_error * params.quantError));
+    bottomPoint = vec4(bottom_new_brightness + (.5 * quant_error * params.quantError));
     textureStore(outputTex, bottomPosition, bottomPoint);
 
     // storageBarrier();
