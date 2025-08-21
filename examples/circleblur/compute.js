89fe71d4bcabe2d1a71534549b71488347954faf
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-08-21 11:40:40 -0600

    circleblur cleanup

--- examples/circleblur/compute.js
+++ examples/circleblur/compute.js
@@ -6,7 +6,6 @@ ${clearMix}
 
 const workgroupSize = 1;
 
-//'function', 'private', 'push_constant', 'storage', 'uniform', 'workgroup'
 
 @compute @workgroup_size(workgroupSize,workgroupSize,1)
 fn main(
@@ -15,25 +14,17 @@ fn main(
     @builtin(local_invocation_id) LocalInvocationID: vec3<u32>
 ) {
 
-    // let filterDim = 128u;
-    // let blockDim = 128u;
-    // let flipValue = 0u;
-
-    // let filterOffset : u32 = (filterDim - 1u) / 2u;
-
-    // let baseIndex = vec2<i32>(
-    //     WorkGroupID.xy * vec2<u32>(blockDim, 4u) +
-    //     LocalInvocationID.xy * vec2<u32>(4u, 1u)
-    // ) - vec2<i32>(i32(filterOffset), 0);
-
-    // ----------------------------------------------
-
 
     var rgba = textureSampleLevel(
-        feedbackTexture,feedbackSampler,
-        vec2f(f32(GlobalId.x), f32(GlobalId.y)),
-        0.0
+        feedbackTexture,
+        feedbackSampler,
+        vec2f(GlobalId.xy),
+        0.
     );
+
+    // textureLoad here behaves differently
+    // var rgba = textureLoad(feedbackTexture, GlobalId.xy, 0);
+
     rgba = clearMix(rgba, 1.01) + vec4f(1.,0.,0., .5);
     textureStore(outputTex, GlobalId.xy, rgba);
 }
