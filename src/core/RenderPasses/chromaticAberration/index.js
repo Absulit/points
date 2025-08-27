f7db4c30522cfa5545fe9d103cd4345d9ede2b5e
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-08-27 16:26:31 -0600

    Chromatic aberration RenderPass updated

--- src/core/RenderPasses/chromaticAberration/index.js
+++ src/core/RenderPasses/chromaticAberration/index.js
@@ -1,17 +1,13 @@
 import vertexShader from './vert.js';
 import fragmentShader from './frag.js';
+import { RenderPass } from 'points';
 
-const chromaticAberration = {
-    vertexShader,
-    fragmentShader,
-    init: async (points, params) => {
-        points.setSampler('renderpass_feedbackSampler', null);
-        points.setTexture2d('renderpass_feedbackTexture', true);
-        points.setUniform('chromaticAberration_distance', params.distance);
-    },
-    update: points => {
-
-    }
-}
+const chromaticAberration = new RenderPass(vertexShader, fragmentShader);
+chromaticAberration.required = ['distance']
+chromaticAberration.setInit((points, params) => {
+    points.setSampler('renderpass_feedbackSampler', null);
+    points.setTexture2d('renderpass_feedbackTexture', true);
+    points.setUniform('chromaticAberration_distance', params.distance);
+});
 
 export default chromaticAberration;
