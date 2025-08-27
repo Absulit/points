9c0898212737c67b42ccf0d88b0a935940cafb7f
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-08-27 16:17:13 -0600

    color RenderPass updated

--- src/core/RenderPasses/color/index.js
+++ src/core/RenderPasses/color/index.js
@@ -1,24 +1,15 @@
 import vertexShader from './vert.js';
 import fragmentShader from './frag.js';
-import Points from 'points';
+import { RenderPass } from 'points';
 
-const color = {
-    vertexShader,
-    fragmentShader,
-    required: ['color', 'blendAmount'],
-    /**
-     * @param {Points} points
-     * @param {*} params
-     */
-    init: async (points, params) => {
-        points.setSampler('renderpass_feedbackSampler', null);
-        points.setTexture2d('renderpass_feedbackTexture', true);
-        points.setUniform('color_blendAmount', params.blendAmount);
-        points.setUniform('color_color', params.color, 'vec4f');
-    },
-    update: points => {
 
-    }
-}
+const color = new RenderPass(vertexShader, fragmentShader);
+color.required = ['color', 'blendAmount'];
+color.setInit((points, params) => {
+    points.setSampler('renderpass_feedbackSampler', null);
+    points.setTexture2d('renderpass_feedbackTexture', true);
+    points.setUniform('color_blendAmount', params.blendAmount);
+    points.setUniform('color_color', params.color, 'vec4f');
+})
 
 export default color;
