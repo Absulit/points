c9003037d8048033ef648632b4c1748cd3022783
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-08-27 15:39:12 -0600

    modified grayscale RenderPass

--- src/core/RenderPasses/grayscale/index.js
+++ src/core/RenderPasses/grayscale/index.js
@@ -1,16 +1,11 @@
 import vertexShader from './vert.js';
 import fragmentShader from './frag.js';
+import { RenderPass } from 'points';
 
-const grayscale = {
-    vertexShader,
-    fragmentShader,
-    init: async (points, params) => {
-        points.setSampler('renderpass_feedbackSampler', null);
-        points.setTexture2d('renderpass_feedbackTexture', true);
-    },
-    update: points => {
-
-    }
-}
+const grayscale = new RenderPass(vertexShader, fragmentShader);
+grayscale.setInit((points, params) => {
+    points.setSampler('renderpass_feedbackSampler', null);
+    points.setTexture2d('renderpass_feedbackTexture', true);
+});
 
 export default grayscale;
