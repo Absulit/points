bf01523471e4a47d5c99436e6b1e1b57385b84dd
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-08-27 18:24:26 -0600

    pixelate RenderPass updated

--- examples/renderpasses2/index.js
+++ examples/renderpasses2/index.js
@@ -10,6 +10,7 @@ const renderpasses1 = {
     renderPasses: [
         new RenderPass(vert1, frag1),
         // new RenderPass(yellow.vertexShader, yellow.fragmentShader),
+        // RenderPasses.GRAYSCALE
     ],
     init: async points => {
         // await yellow.init(points, {blendAmount: .5});
@@ -24,7 +25,7 @@ const renderpasses1 = {
         points.addRenderPass(RenderPasses.GRAYSCALE);
         points.addRenderPass(RenderPasses.CHROMATIC_ABERRATION, { distance: .02 });
         points.addRenderPass(RenderPasses.COLOR, {color:[.5, 1, 0, 1], blendAmount: .5});
-        // points.addRenderPass(RenderPasses.PIXELATE, { pixelsWidth: 10, pixelsHeight: 10 });
+        points.addRenderPass(RenderPasses.PIXELATE, { pixelsWidth: 10, pixelsHeight: 10 });
         // points.addRenderPass(RenderPasses.LENS_DISTORTION, { amount: .4, distance: .01 });
         // points.addRenderPass(RenderPasses.FILM_GRAIN);
         // points.addRenderPass(RenderPasses.BLOOM, { amount: .5 });
--- src/core/RenderPasses/pixelate/index.js
+++ src/core/RenderPasses/pixelate/index.js
@@ -1,18 +1,14 @@
 import vertexShader from './vert.js';
 import fragmentShader from './frag.js';
+import { RenderPass } from 'points';
 
-const pixelate = {
-    vertexShader,
-    fragmentShader,
-    init: async (points, params) => {
-        points.setSampler('renderpass_feedbackSampler', null);
-        points.setTexture2d('renderpass_feedbackTexture', true);
-        points.setUniform('pixelate_pixelsWidth', params.pixelsWidth);
-        points.setUniform('pixelate_pixelsHeight', params.pixelsHeight);
-    },
-    update: points => {
-
-    }
-}
+const pixelate = new RenderPass(vertexShader, fragmentShader);
+pixelate.required = ['pixelsWidth', 'pixelsHeight']
+pixelate.setInit((points, params) => {
+    points.setSampler('renderpass_feedbackSampler', null);
+    points.setTexture2d('renderpass_feedbackTexture', true);
+    points.setUniform('pixelate_pixelsWidth', params.pixelsWidth);
+    points.setUniform('pixelate_pixelsHeight', params.pixelsHeight);
+});
 
 export default pixelate;
