daeee62bc477fa68577085112648d9c3fb3cc04d
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-08-27 18:27:45 -0600

    lensDistortion RenderPass updated

--- src/core/RenderPasses/lensDistortion/index.js
+++ src/core/RenderPasses/lensDistortion/index.js
@@ -1,18 +1,14 @@
 import vertexShader from './vert.js';
 import fragmentShader from './frag.js';
+import { RenderPass } from 'points';
 
-const lensDistortion = {
-    vertexShader,
-    fragmentShader,
-    init: async (points, params) => {
-        points.setSampler('renderpass_feedbackSampler', null);
-        points.setTexture2d('renderpass_feedbackTexture', true);
-        points.setUniform('lensDistortion_amount', params?.amount || .4)
-        points.setUniform('lensDistortion_distance', params?.distance || .01);
-    },
-    update: async points => {
-
-    }
-}
+const lensDistortion = new RenderPass(vertexShader, fragmentShader);
+lensDistortion.required = ['amount', 'distance'];
+lensDistortion.setInit((points, params) => {
+    points.setSampler('renderpass_feedbackSampler', null);
+    points.setTexture2d('renderpass_feedbackTexture', true);
+    points.setUniform('lensDistortion_amount', params?.amount || .4)
+    points.setUniform('lensDistortion_distance', params?.distance || .01);
+});
 
 export default lensDistortion;
