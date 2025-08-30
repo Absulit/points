07a2c3ef7286141dffdf3f7c1b3e4de2bd58dfce
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-08-29 19:33:41 -0600

    Update RenderPasses.js

--- src/RenderPasses.js
+++ src/RenderPasses.js
@@ -12,26 +12,29 @@ import RenderPass from './RenderPass.js';
 
 /**
  * List of predefined Render Passes for Post Processing.
+ * Parameters required are shown as a warning in the JS console.
  * @class
  *
  * @example
  * import Points, { RenderPass, RenderPasses } from 'points';
  * const points = new Points('canvas');
  *
+ * // option 1: along with the RenderPasses pased into `Points.init()`
  * let renderPasses = [
  *     new RenderPass(vert1, frag1, compute1),
  *     new RenderPass(vert2, frag2, compute2)
  * ];
  *
- * RenderPasses.grayscale(points);
- * RenderPasses.chromaticAberration(points, .02);
- * RenderPasses.color(points, .5, 1, 0, 1, .5);
- * RenderPasses.pixelate(points, 10, 10);
- * RenderPasses.lensDistortion(points, .4, .01);
- * RenderPasses.filmgrain(points);
- * RenderPasses.bloom(points, .5);
- * RenderPasses.blur(points, 100, 100, .4, 0, 0.0);
- * RenderPasses.waves(points, .05, .03);
+ * // option 2: calling `points.addRenderPass()` method
+ * points.addRenderPass(RenderPasses.GRAYSCALE);
+ * points.addRenderPass(RenderPasses.CHROMATIC_ABERRATION, { distance: .02 });
+ * points.addRenderPass(RenderPasses.COLOR, { color: [.5, 1, 0, 1], blendAmount: .5 });
+ * points.addRenderPass(RenderPasses.PIXELATE);
+ * points.addRenderPass(RenderPasses.LENS_DISTORTION);
+ * points.addRenderPass(RenderPasses.FILM_GRAIN);
+ * points.addRenderPass(RenderPasses.BLOOM);
+ * points.addRenderPass(RenderPasses.BLUR, { resolution: [100, 100], direction: [.4, 0], radians: 0 });
+ * points.addRenderPass(RenderPasses.WAVES, { scale: .05 });
  *
  * await points.init(renderPasses);
  *
