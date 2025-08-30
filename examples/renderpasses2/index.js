87627e04c01fe246d916677b8bf8170567198d2a
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-08-29 18:26:31 -0600

    chromatic aberration updated

--- examples/renderpasses2/index.js
+++ examples/renderpasses2/index.js
@@ -29,10 +29,10 @@ const renderpasses1 = {
 
         // TODO: agregar documentación de nuevo parámetro isolated en set*
         // TODO: checl setAudio since it doesn't return a isolated
-        points.addRenderPass(RenderPasses.GRAYSCALE);
-        // points.addRenderPass(RenderPasses.CHROMATIC_ABERRATION, { distance: .02 });
+        // points.addRenderPass(RenderPasses.GRAYSCALE);
+        points.addRenderPass(RenderPasses.CHROMATIC_ABERRATION, { distance: .02 });
         // points.addRenderPass(RenderPasses.COLOR, { color: [.5, 1, 0, 1], blendAmount: .5 });
-        points.addRenderPass(RenderPasses.PIXELATE);
+        // points.addRenderPass(RenderPasses.PIXELATE);
         // points.addRenderPass(RenderPasses.LENS_DISTORTION);
         // points.addRenderPass(RenderPasses.FILM_GRAIN);
         // points.addRenderPass(RenderPasses.BLOOM);
--- src/core/RenderPasses/chromaticAberration/index.js
+++ src/core/RenderPasses/chromaticAberration/index.js
@@ -3,10 +3,14 @@ import fragmentShader from './frag.js';
 import { RenderPass } from 'points';
 
 const chromaticAberration = new RenderPass(vertexShader, fragmentShader, null, 8, 8, 1, (points, params) => {
+    points.isolated = true;
     points.setSampler('renderpass_feedbackSampler', null);
     points.setTexture2d('renderpass_feedbackTexture', true);
     points.setUniform('chromaticAberration_distance', params.distance || .02);
+    points.isolated = false;
 });
-chromaticAberration.required = ['distance']
+chromaticAberration.required = ['distance'];
+chromaticAberration.name = 'chromaticAberration';
+chromaticAberration.isolated = true;
 
 export default chromaticAberration;
