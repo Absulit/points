b9ae350b4c16e6e2e8e58ee16b354a96fd9af1a9
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-08-30 18:20:04 -0600

    80 chars limit
    another 80 chars limit
    
    Update frag.js

--- examples/classicnoise1/frag.js
+++ examples/classicnoise1/frag.js
@@ -26,10 +26,19 @@ fn main(
 ) -> @location(0) vec4f {
 
     let globalScale = .5;
-    let n0 = cnoise(rotateVector(uvr + params.time * .01, TAU * .25 + params.time * .01) * 50 * globalScale) * .5 + .5;
-    let n1 = cnoise(rotateVector(uvr, TAU * .75 + params.time * .03) * 100 * globalScale) * .5 + .5;
-    let n2 = cnoise(rotateVector(uvr, TAU * .95 + params.time * .04) * 150 * globalScale) * .5 + .5;
-    let finalColor = layer(vec4f(0,.5,.5,1), layer(n0 * GREEN, layer(BLUE * n2, WHITE * n1)));
+    let n0 = cnoise(
+        rotateVector(uvr + params.time * .01, TAU * .25 + params.time * .01) *
+        50 * globalScale
+    ) * .5 + .5;
+    let n1 = cnoise(
+        rotateVector(uvr, TAU * .75 + params.time * .03) * 100 * globalScale
+    ) * .5 + .5;
+    let n2 = cnoise(
+        rotateVector(uvr, TAU * .95 + params.time * .04) * 150 * globalScale
+    ) * .5 + .5;
+    let layer0 = layer(BLUE * n2, WHITE * n1);
+    let layer1 = layer(n0 * GREEN, layer0);
+    let finalColor = layer(vec4f(0, .5, .5, 1), layer1);
 
     return finalColor;
 }
