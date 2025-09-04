2861e996139dbf12d8431c1d6b203da8f2c258c6
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-09-04 15:57:53 -0600

    face detection scaffold

---
+++ examples/face_detection_1/index.js
@@ -0,0 +1,50 @@
+import vert from './vert.js';
+import compute from './compute.js';
+import frag from './frag.js';
+import Points from 'points';
+
+const options = {
+    val: 0,
+    bool: false,
+    color1: '#FF0000', // CSS string
+    color2: [0, 128, 255], // RGB array
+    color3: [0, 128, 255, 0.3], // RGB with alpha
+    color4: { h: 350, s: 0.9, v: 0.3 }, // Hue, saturation, value
+    color5: { r: 115, g: 50.9, b: 20.3, a: .1 }, // r, g, b object
+}
+
+const base = {
+    vert,
+    compute,
+    frag,
+    /**
+     * @param {Points} points
+     */
+    init: async (points, folder) => {
+
+        // Add elements to dat gui
+        // create an uniform and get value from options
+        points.setUniform('val', options.val);
+
+        // https://github.com/dataarts/dat.gui/blob/master/API.md#GUI+add
+        folder.add(options, 'val', -1, 1, .0001).name('Val');
+        folder.add(options, 'bool').name('Bool');
+
+        // https://github.com/dataarts/dat.gui/blob/master/API.md#GUI+addColor
+        folder.addColor(options, 'color1');
+        folder.addColor(options, 'color2');
+        folder.addColor(options, 'color3');
+        folder.addColor(options, 'color4');
+        folder.addColor(options, 'color5');
+
+        folder.open();
+    },
+    /**
+     * @param {Points} points
+     */
+    update: points => {
+        points.setUniform('val', options.val);
+    }
+}
+
+export default base;
