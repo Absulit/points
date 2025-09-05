950d662b559e2fb0a83f58a95efe987bf478d583
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-09-05 08:33:28 -0600

    cleaning code

--- examples/face_detection_1/lpb_renderpass/compute.js
+++ examples/face_detection_1/lpb_renderpass/compute.js
@@ -1,28 +1,5 @@
 const compute = /*wgsl*/`
 
-
-
-// function computeLBP(gray, width, height) {
-//   const lbp = new Uint8Array(width * height);
-//   for (let y = 1; y < height - 1; y++) {
-//     for (let x = 1; x < width - 1; x++) {
-//       const center = gray[y * width + x];
-//       let code = 0;
-//       let idx = 0;
-//       for (let dy = -1; dy <= 1; dy++) {
-//         for (let dx = -1; dx <= 1; dx++) {
-//           if (dx === 0 && dy === 0) continue;
-//           const neighbor = gray[(y + dy) * width + (x + dx)];
-//           code |= (neighbor >= center ? 1 : 0) << idx;
-//           idx++;
-//         }
-//       }
-//       lbp[y * width + x] = code;
-//     }
-//   }
-//   return lbp;
-// }
-
 const SIZE = vec2u(800, 800);
 
 fn pack_bools_to_f32(bits: array<bool, 8>) -> f32 {
@@ -42,9 +19,9 @@ fn pack_bools_to_f32(bits: array<bool, 8>) -> f32 {
 
 @compute @workgroup_size(1,1,1)
 fn main(
-    @builtin(global_invocation_id) GlobalId: vec3<u32>,
-    @builtin(workgroup_id) WorkGroupID: vec3<u32>,
-    @builtin(local_invocation_id) LocalInvocationID: vec3<u32>
+    @builtin(global_invocation_id) GlobalId: vec3u,
+    @builtin(workgroup_id) WorkGroupID: vec3u,
+    @builtin(local_invocation_id) LocalInvocationID: vec3u
 ) {
     let grayscale = textureLoad(grayscalePassTexture, GlobalId.xy, 0); // image
 
