e64c69c3c1d72550981ce2ec6aeee3b029915ff0
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-09-05 16:23:30 -0600

    renamed

--- examples/face_detection_1/compare_histograms_renderpass/index.js
+++ examples/face_detection_1/compare_histograms_renderpass/index.js
@@ -1,5 +1,5 @@
 import { RenderPass } from "points";
 import compute from "./compute.js";
 
-const compare_histogram_renderpass = new RenderPass(null, null, compute, 800, 800, 1);
-export default compare_histogram_renderpass;
+const compare_histograms_renderpass = new RenderPass(null, null, compute, 800, 800, 1);
+export default compare_histograms_renderpass;
