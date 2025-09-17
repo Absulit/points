ca4c8f8721d05f4e01f01ecd358349c5f0df3923
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-09-16 18:47:58 -0600

    table explainer

--- src/storage-accessmode.js
+++ src/storage-accessmode.js
@@ -1,3 +1,27 @@
+/**
+
+The idea here is that the columns are the current stage of the storage
+being checked at entries and dynamic bindings, and the rows are the stage where
+the storage should show; the table then matches both.
+The thing to remember here is, if the storage is required in any combination,
+then the fragment stage (if is included) then there the storage must be
+read access mode; if there's no vertex then the storage during fragment can be
+read_write. Compute is always read_write.
+
+
+| storage      \     current| COMPUTE    | VERTEX	 | FRAGMENT
+| --------------------------|:-----------|:----------|----------:|
+| compute, vertex, fragment | read_write | read	     | read
+| compute                   | read_write |           |
+| vertex                    |            | read      |
+| fragment                  |            |           | read_write
+| compute, vertex           | read_write | read      |
+| compute, fragment         | read_write |           | read_write
+| vertex, fragment          |            | read	     | read
+
+
+*/
+
 const cache = {
     [GPUShaderStage.COMPUTE | GPUShaderStage.VERTEX | GPUShaderStage.FRAGMENT]: {
         [GPUShaderStage.COMPUTE]: 'rw',
