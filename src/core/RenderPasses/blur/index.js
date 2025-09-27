05bf55d3e5083bc77b5ab6cbecf3737f2f59f521
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-09-26 19:34:14 -0600

    internal sets in RenderPasses
    - to not share the buffer with the other passes

--- src/core/RenderPasses/blur/index.js
+++ src/core/RenderPasses/blur/index.js
@@ -3,8 +3,8 @@ import fragmentShader from './frag.js';
 import { RenderPass } from 'points';
 
 const blur = new RenderPass(vertexShader, fragmentShader, null, 8, 8, 1, (points, params) => {
-    points.setSampler('renderpass_feedbackSampler', null);
-    points.setTexture2d('renderpass_feedbackTexture', true);
+    points.setSampler('renderpass_feedbackSampler', null).internal = true;
+    points.setTexture2d('renderpass_feedbackTexture', true).internal = true;
     points.setUniform('blur_resolution', params.resolution || [50, 50], 'vec2f');
     points.setUniform('blur_direction', params.direction || [.4, .4], 'vec2f');
     points.setUniform('blur_radians', params.radians || 0);
--- src/core/RenderPasses/lensDistortion/index.js
+++ src/core/RenderPasses/lensDistortion/index.js
@@ -5,8 +5,8 @@ import { RenderPass } from 'points';
 const lensDistortion = new RenderPass(vertexShader, fragmentShader, null, 8, 8, 1, (points, params) => {
     points.setSampler('renderpass_feedbackSampler', null).internal = true;
     points.setTexture2d('renderpass_feedbackTexture', true).internal = true;
-    points.setUniform('lensDistortion_amount', params.amount || .4).internal = true;
-    points.setUniform('lensDistortion_distance', params.distance || .01).internal = true;
+    points.setUniform('lensDistortion_amount', params.amount || .4);
+    points.setUniform('lensDistortion_distance', params.distance || .01);
 });
 lensDistortion.required = ['amount', 'distance'];
 lensDistortion.name = 'Lens Distortion';
