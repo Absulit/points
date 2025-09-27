2e67634cf9c4210a81169a6cca82d11b00e005c4
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-09-26 19:35:41 -0600

    Update index.js

--- src/core/RenderPasses/grayscale/index.js
+++ src/core/RenderPasses/grayscale/index.js
@@ -3,8 +3,8 @@ import fragmentShader from './frag.js';
 import { RenderPass } from 'points';
 
 const grayscale = new RenderPass(vertexShader, fragmentShader, null, 8, 8, 1, (points, params) => {
-    points.setSampler('renderpass_feedbackSampler', null);
-    points.setTexture2d('renderpass_feedbackTexture', true);
+    points.setSampler('renderpass_feedbackSampler', null).internal = true;
+    points.setTexture2d('renderpass_feedbackTexture', true).internal = true;
 });
 grayscale.name = 'Grayscale';
 
