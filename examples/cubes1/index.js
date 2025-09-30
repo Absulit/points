7d37f9e24bc3bc48387dd715b53a45959b74d3b9
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-09-30 08:45:42 -0600

    particle data

--- examples/cubes1/index.js
+++ examples/cubes1/index.js
@@ -11,6 +11,17 @@ const options = {
     color5: { r: 115, g: 50.9, b: 20.3, a: .1 }, // r, g, b object
 }
 
+const WORKGROUP_X = 1;
+const WORKGROUP_Y = 1;
+const WORKGROUP_Z = 1;
+
+const THREADS_X = 1;
+const THREADS_Y = 1;
+const THREADS_Z = 1;
+
+const NUMPARTICLES = WORKGROUP_X * WORKGROUP_Y * WORKGROUP_Z * THREADS_X * THREADS_Y * THREADS_Z;
+console.log('NUMPARTICLES: ', NUMPARTICLES);
+
 
 const near = 0.1, far = 100;
 const f = 1.0 / Math.tan(Math.PI / 8); // ≈ 2.414
@@ -57,20 +68,8 @@ const base = {
         )
 
 
-        // Add elements to dat gui
-        // create an uniform and get value from options
         points.setUniform('val', options.val);
-
-        // https://github.com/dataarts/dat.gui/blob/master/API.md#GUI+add
         folder.add(options, 'val', -1, 1, .0001).name('Val');
-        folder.add(options, 'bool').name('Bool');
-
-        // https://github.com/dataarts/dat.gui/blob/master/API.md#GUI+addColor
-        folder.addColor(options, 'color1');
-        folder.addColor(options, 'color2');
-        folder.addColor(options, 'color3');
-        folder.addColor(options, 'color4');
-        folder.addColor(options, 'color5');
 
         folder.open();
 
--- examples/cubes1/structs.js
+++ examples/cubes1/structs.js
@@ -2,6 +2,8 @@ export const structs = /*wgsl*/`
 
 struct Particle {
     position: vec3f,
+    rotation: vec3f,
+    color: vec3f,
 }
 
 `;
