f413ec9579509109181422261ad33c9f5371f5f9
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-10-01 10:59:28 -0600

    removing cast

--- src/core/defaultFunctions.js
+++ src/core/defaultFunctions.js
@@ -37,8 +37,8 @@ fn defaultVertexBody(position: vec4f, color: vec4f, uv: vec2f) -> Fragment {
     let ratioX = params.screen.x / params.screen.y;
     let ratioY = 1. / ratioX / (params.screen.y / params.screen.x);
     result.ratio = vec2(ratioX, ratioY);
-    result.position = vec4f(position);
-    result.color = vec4f(color);
+    result.position = position;
+    result.color = color;
     result.uv = uv;
     result.uvr = vec2(uv.x * result.ratio.x, uv.y);
     result.mouse = vec2(params.mouse.x / params.screen.x, params.mouse.y / params.screen.y);
