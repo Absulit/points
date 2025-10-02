9d6758c4fb7fdf5c9b96eb33a266aa0671d2c399
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-10-02 15:59:33 -0600

    cleaning and taxonomy

--- examples/cube1/cube_renderpass/index.js
+++ examples/cube1/cube_renderpass/index.js
@@ -1,6 +1,5 @@
 import { RenderPass } from 'points';
 import vert from './vert.js';
 import frag from './frag.js';
-import compute from './compute.js';
 
-export const cube_renderpass = new RenderPass(vert, frag, compute);
+export const cube_renderpass = new RenderPass(vert, frag);
--- examples/cube1/index.js
+++ examples/cube1/index.js
@@ -4,13 +4,7 @@ import { cube_renderpass } from './cube_renderpass/index.js';
 import { staticcube_renderpass } from './staticcube_renderpass/index.js';
 
 const options = {
-    val: 0,
-    bool: false,
-    color1: '#FF0000', // CSS string
-    color2: [0, 128, 255], // RGB array
-    color3: [0, 128, 255, 0.3], // RGB with alpha
-    color4: { h: 350, s: 0.9, v: 0.3 }, // Hue, saturation, value
-    color5: { r: 115, g: 50.9, b: 20.3, a: .1 }, // r, g, b object
+
 }
 
 
@@ -92,25 +86,6 @@ const base = {
         // points.addRenderPass(RenderPasses.COLOR);
         // points.addRenderPass(RenderPasses.PIXELATE);
         // points.addRenderPass(RenderPasses.FILM_GRAIN);
-
-        // Add elements to dat gui
-        // create an uniform and get value from options
-        points.setUniform('val', options.val);
-
-        // https://github.com/dataarts/dat.gui/blob/master/API.md#GUI+add
-        folder.add(options, 'val', -1, 1, .0001).name('Val');
-        folder.add(options, 'bool').name('Bool');
-
-        // https://github.com/dataarts/dat.gui/blob/master/API.md#GUI+addColor
-        folder.addColor(options, 'color1');
-        folder.addColor(options, 'color2');
-        folder.addColor(options, 'color3');
-        folder.addColor(options, 'color4');
-        folder.addColor(options, 'color5');
-
-        folder.open();
-
-
     },
     /**
      * @param {Points} points
@@ -127,8 +102,6 @@ const base = {
                 0, 0, (2 * far * near) * nf, 0
             ]
         )
-
-        points.setUniform('val', options.val);
     }
 }
 
