400254b13c55a8599f03f975f030ba8e5469c7f7
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-10-04 15:24:41 -0600

    Update frag.js

--- examples/particles4/cube_renderpass/frag.js
+++ examples/particles4/cube_renderpass/frag.js
@@ -12,16 +12,8 @@ fn main(
     input: CustomFragment
 ) -> @location(0) vec4f {
 
-    let cellSize = 20. + 10. * fnusin(1.);
-    let a = sin(input.uvr.x  * cellSize) * sin(input.uvr.y * cellSize);
-    let b = sin(input.uvr.x * input.uvr.y * 10. * 9.1 * .25 );
-    let c = fnusin(input.uvr.x * input.uvr.y * 10.);
-    let d = distance(a,b);
-    let f = d * input.uvr.x * input.uvr.y;
-    let finalColor:vec4f = vec4(f,f*c*a,f, 1.);
-
     let depth = clamp(input.depth.r * 8, 0, 1);
-    return vec4f(finalColor.rgb * (1-depth), 1);
+    return vec4f(input.color.rgb * (1-depth), 1);
 }
 `;
 
