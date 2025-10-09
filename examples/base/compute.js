79f935c638df1232c4dc8d125d733f011f4c8df5
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-10-09 14:08:37 -0600

    Update compute.js

--- examples/base/compute.js
+++ examples/base/compute.js
@@ -2,9 +2,9 @@ const compute = /*wgsl*/`
 
 @compute @workgroup_size(8,8,1)
 fn main(
-    @builtin(global_invocation_id) GlobalId: vec3<u32>,
-    @builtin(workgroup_id) WorkGroupID: vec3<u32>,
-    @builtin(local_invocation_id) LocalInvocationID: vec3<u32>
+    @builtin(global_invocation_id) GlobalId: vec3u,
+    @builtin(workgroup_id) WorkGroupID: vec3u,
+    @builtin(local_invocation_id) LocalInvocationID: vec3u
 ) {
     let time = params.time;
 }
