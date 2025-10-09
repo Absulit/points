2bae6bf059e319b760c3cf1ad814e799d5eac43d
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-10-09 14:44:41 -0600

    sphere spinning

--- examples/mesh_displace_1/index.js
+++ examples/mesh_displace_1/index.js
@@ -6,15 +6,15 @@ import Points, { RenderPass } from 'points';
 const options = {
     val: 0,
     bool: false,
-    color1: '#FF0000', // CSS string
-    color2: [0, 128, 255], // RGB array
-    color3: [0, 128, 255, 0.3], // RGB with alpha
-    color4: { h: 350, s: 0.9, v: 0.3 }, // Hue, saturation, value
-    color5: { r: 115, g: 50.9, b: 20.3, a: .1 }, // r, g, b object
 }
 
+const near = 0.1, far = 100;
+const f = 1.0 / Math.tan(Math.PI / 8); // ≈ 2.414
+let aspect = null
+const nf = 1 / (near - far);
+
 const renderPass = new RenderPass(vert, frag, compute);
-renderPass.addSphere('sphere', { x: 0, y: 0, z: 0 }, { r: 1, g: 1, b: 0, a: 1 });
+renderPass.addSphere('sphere', { x: 0, y: 0, z: 0 }, { r: 1, g: 1, b: 0, a: 1 }, 1, 32, 32);
 
 const base = {
     renderPasses: [renderPass],
@@ -23,20 +23,34 @@ const base = {
      */
     init: async (points, folder) => {
 
-        // Add elements to dat gui
-        // create an uniform and get value from options
-        points.setUniform('val', options.val);
+        aspect = points.canvas.width / points.canvas.height;
+        points.setUniform(
+            'projection',
+            [
+                f / aspect, 0, 0, 0,
+                0, f, 0, 0,
+                0, 0, (far + near) * nf, -1,
+                0, 0, (2 * far * near) * nf, 0
+            ],
+            'mat4x4<f32>'
+        )
 
-        // https://github.com/dataarts/dat.gui/blob/master/API.md#GUI+add
+        // camera at [0, 0, 5], looking at origin
+        points.setUniform(
+            'view',
+            [
+                1, 0, 0, 0,
+                0, 1, 0, 0,
+                0, 0, 1, 0,
+                0, 0, -5, 1
+            ],
+            'mat4x4<f32>'
+        )
+
+
+        points.setUniform('val', options.val);
         folder.add(options, 'val', -1, 1, .0001).name('Val');
         folder.add(options, 'bool').name('Bool');
-
-        // https://github.com/dataarts/dat.gui/blob/master/API.md#GUI+addColor
-        folder.addColor(options, 'color1');
-        folder.addColor(options, 'color2');
-        folder.addColor(options, 'color3');
-        folder.addColor(options, 'color4');
-        folder.addColor(options, 'color5');
 
         folder.open();
     },
@@ -44,6 +58,18 @@ const base = {
      * @param {Points} points
      */
     update: points => {
+
+        aspect = points.canvas.width / points.canvas.height;
+        points.setUniform(
+            'projection',
+            [
+                f / aspect, 0, 0, 0,
+                0, f, 0, 0,
+                0, 0, (far + near) * nf, -1,
+                0, 0, (2 * far * near) * nf, 0
+            ]
+        )
+
         points.setUniform('val', options.val);
     }
 }
--- examples/mesh_displace_1/vert.js
+++ examples/mesh_displace_1/vert.js
@@ -1,4 +1,10 @@
+import { rotXAxis, rotYAxis, rotZAxis } from 'points/math';
+
 const vert = /*wgsl*/`
+
+${rotXAxis}
+${rotYAxis}
+${rotZAxis}
 
 @vertex
 fn main(
@@ -8,8 +14,24 @@ fn main(
     @location(3) normal: vec3f,
     @builtin(vertex_index) vertexIndex: u32
 ) -> Fragment {
+    var angleZ = params.time * 0.9854;
+    var angleY = params.time * 0.94222;
+    var angleX = params.time * 0.865;
 
-    return defaultVertexBody(position, color, uv, normal);
+    let rotX = rotXAxis(angleX);
+    let rotY = rotYAxis(angleY);
+    let rotZ = rotZAxis(angleZ);
+    let model = rotX * rotY * rotZ;
+
+    let world = (model * vec4f(position.xyz, 1.)).xyz;
+    let clip = params.projection * params.view * vec4f(world, 1.);
+
+    let newNormal = normalize((model * vec4f(normal, 0.)).xyz);
+
+    var dvb = defaultVertexBody(clip, color, uv, newNormal);
+    // dvb.id = id;
+
+    return dvb;
 }
 `;
 
