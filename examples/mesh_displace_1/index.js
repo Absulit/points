17c800fcfe988be3c4004daf6ff818fa57494c30
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-10-12 16:29:29 -0600

    Update index.js

--- examples/mesh_displace_1/index.js
+++ examples/mesh_displace_1/index.js
@@ -16,6 +16,7 @@ const nf = 1 / (near - far);
 const renderPass = new RenderPass(vert, frag, compute);
 renderPass.clearValue = { r: 61 / 255, g: 37 / 255, b: 103 / 255, a: 1 }
 renderPass.addSphere('sphere', { x: 0, y: 0, z: 0 }, { r: 1, g: 1, b: 0, a: 1 }, 1.6, 256, 256);
+renderPass.depthWriteEnabled = true;
 
 const base = {
     renderPasses: [renderPass],
