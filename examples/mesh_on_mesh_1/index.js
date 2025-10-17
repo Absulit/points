ce501362966b815bcf27abb94c94cecf8253cdd7
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-10-16 19:04:02 -0600

    adding perspective and 4 spheres

--- examples/mesh_on_mesh_1/index.js
+++ examples/mesh_on_mesh_1/index.js
@@ -6,16 +6,28 @@ import Points, { RenderPass } from 'points';
 const options = {
     val: 0,
     bool: false,
-    color1: '#FF0000', // CSS string
-    color2: [0, 128, 255], // RGB array
-    color3: [0, 128, 255, 0.3], // RGB with alpha
-    color4: { h: 350, s: 0.9, v: 0.3 }, // Hue, saturation, value
-    color5: { r: 115, g: 50.9, b: 20.3, a: .1 }, // r, g, b object
 }
 
+const WORKGROUP_X = 1;
+const WORKGROUP_Y = 1;
+const WORKGROUP_Z = 1;
+
+const THREADS_X = 4;
+const THREADS_Y = 1;
+const THREADS_Z = 1;
+
+const NUMPARTICLES = WORKGROUP_X * WORKGROUP_Y * WORKGROUP_Z * THREADS_X * THREADS_Y * THREADS_Z;
+console.log('NUMPARTICLES: ', NUMPARTICLES);
+
 const renderPass = new RenderPass(vert, frag, compute);
+renderPass.depthWriteEnabled = true;
 renderPass.addCube('base_mesh').instanceCount = 1;
-// renderPass.addSphere('instance_mesh').instanceCount = 1;
+renderPass.addSphere('instance_mesh', {x:0,y:1,z:0},{r:0,g:0,b:0,a:0},.1).instanceCount = NUMPARTICLES;
+
+const near = 0.1, far = 100;
+const f = 1.0 / Math.tan(Math.PI / 8); // ≈ 2.414
+let aspect = null
+const nf = 1 / (near - far);
 
 const base = {
     renderPasses: [
@@ -26,20 +38,34 @@ const base = {
      */
     init: async (points, folder) => {
 
-        // Add elements to dat gui
-        // create an uniform and get value from options
         points.setUniform('val', options.val);
 
-        // https://github.com/dataarts/dat.gui/blob/master/API.md#GUI+add
         folder.add(options, 'val', -1, 1, .0001).name('Val');
         folder.add(options, 'bool').name('Bool');
 
-        // https://github.com/dataarts/dat.gui/blob/master/API.md#GUI+addColor
-        folder.addColor(options, 'color1');
-        folder.addColor(options, 'color2');
-        folder.addColor(options, 'color3');
-        folder.addColor(options, 'color4');
-        folder.addColor(options, 'color5');
+        aspect = points.canvas.width / points.canvas.height;
+        points.setUniform(
+            'projection',
+            [
+                f / aspect, 0, 0, 0,
+                0, f, 0, 0,
+                0, 0, (far + near) * nf, -1,
+                0, 0, (2 * far * near) * nf, 0
+            ],
+            'mat4x4<f32>'
+        )
+
+        // camera at [0, 0, 5], looking at origin
+        points.setUniform(
+            'view',
+            [
+                1, 0, 0, 0,
+                0, 1, 0, 0,
+                0, 0, 1, 0,
+                0, 0, -5, 1
+            ],
+            'mat4x4<f32>'
+        )
 
         folder.open();
     },
