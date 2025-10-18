526aa11a3d7d9ddaecffc0fb497f84b1267aaf6a
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-10-18 10:14:23 -0600

    cleanup

--- examples/mesh_on_mesh_1/vert.js
+++ examples/mesh_on_mesh_1/vert.js
@@ -31,34 +31,24 @@ fn main(
         return Fragment();
     }
 
-    var angleZ = params.time * 0.9854;
-    var angleY = params.time * 0.94222;
-    var angleX = params.time * 0.865;
+    // let angleZ = params.time * 0.9854;
+    let angleY = params.time * 0.94222;
+    // let angleX = params.time * 0.865;
 
-    // if(id == mesh.base_mesh){
-    //     angleZ = params.time * 0.1854;
-    //     angleY = params.time * 0.694222;
-    //     angleX = params.time * 0.4865;
-    // }
-
-    var p = vec4f();
-    if(instanceIndex >= 0){
-        p = vertex_data[instanceIndex];
-    }
+    let p = vertex_data[instanceIndex];
 
     let rotX = rotXAxis(0);
     let rotY = rotYAxis(angleY);
     let rotZ = rotZAxis(0);
     var model = rotX * rotY * rotZ;
 
-    var rotated = model * (position);
+    var rotated = model * position;
     if(mesh.instance_mesh == id){
         model = rotX * rotY * rotZ * translationMatrix(p.xyz);
         rotated = model * position;
     }
-    let scaled = rotated * 1;//scale;
-    let world = scaled;
 
+    let world = rotated;
     let clip = params.projection * params.view * world;
 
     let newNormal = normalize((model * vec4f(normal, 0.)).xyz);
