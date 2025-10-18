adbdfddc99055d735c3ce58a2671bd248ade96bc
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-10-18 14:02:50 -0600

    glb1 tags

--- examples/glb1/index.js
+++ examples/glb1/index.js
@@ -3,9 +3,6 @@ import Points, { RenderPass, RenderPasses } from 'points';
 import { cube_renderpass } from './cube_renderpass/index.js';
 import { loadAndExtract } from 'utils';
 
-
-
-
 const options = {
     mode: 1
 }
@@ -15,17 +12,12 @@ const f = 1.0 / Math.tan(Math.PI / 8); // ≈ 2.414
 let aspect = null
 const nf = 1 / (near - far);
 
-
-
 const url = '../models/monkey.glb'; // or remote URL (CORS must allow)
 const data = await loadAndExtract(url);
 const { positions, colors, uvs, normals, indices, colorSize, texture } = data[0]
 cube_renderpass.addMesh('monkey', positions, colors, colorSize, uvs, normals, indices)
 cube_renderpass.depthWriteEnabled = true;
 cube_renderpass.clearValue = { r: 61 / 255, g: 37 / 255, b: 103 / 255, a: 1 }
-
-const textureOut = texture;
-
 
 const base = {
     renderPasses: [
@@ -35,7 +27,7 @@ const base = {
      * @param {Points} points
      */
     init: async (points, folder) => {
-        await points.setTextureImage('albedo', textureOut);
+        await points.setTextureImage('albedo', texture);
         points.setSampler('imageSampler', null);
 
         const dropdownItems = { /*'Vertex': 0,*/ 'Texture': 1, 'Shader': 2 };
