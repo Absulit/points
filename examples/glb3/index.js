cd9ea6917cac4ef012b938f412b352e23e4c2ce5
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-10-18 14:32:07 -0600

    glb3 tags
    and renderpass rename

--- examples/glb3/index.js
+++ examples/glb3/index.js
@@ -1,6 +1,6 @@
 
 import Points, { RenderPass, RenderPasses } from 'points';
-import { cube_renderpass } from './cube_renderpass/index.js';
+import { mesh_renderpass } from './mesh_renderpass/index.js';
 import { loadAndExtract } from 'utils';
 
 const options = {
@@ -12,27 +12,22 @@ const f = 1.0 / Math.tan(Math.PI / 8); // ≈ 2.414
 let aspect = null
 const nf = 1 / (near - far);
 
-
-
 const url = '../models/monkey_subdivide.glb'; // or remote URL (CORS must allow)
 const data = await loadAndExtract(url);
 const { positions, colors, uvs, normals, indices, colorSize, texture } = data[0]
-cube_renderpass.addMesh('monkey', positions, colors, colorSize, uvs, normals, indices);
-cube_renderpass.depthWriteEnabled = true;
-cube_renderpass.clearValue = { r: 61 / 255, g: 37 / 255, b: 103 / 255, a: 1 }
-
-const textureOut = texture;
-
+mesh_renderpass.addMesh('monkey', positions, colors, colorSize, uvs, normals, indices);
+mesh_renderpass.depthWriteEnabled = true;
+mesh_renderpass.clearValue = { r: 61 / 255, g: 37 / 255, b: 103 / 255, a: 1 }
 
 const base = {
     renderPasses: [
-        cube_renderpass,
+        mesh_renderpass,
     ],
     /**
      * @param {Points} points
      */
     init: async (points, folder) => {
-        await points.setTextureImage('albedo', textureOut);
+        await points.setTextureImage('albedo', texture);
         points.setSampler('imageSampler', null);
 
         const dropdownItems = { /*'Vertex': 0,*/ 'Texture': 1, 'Shader': 2 };
--- examples/glb3/mesh_renderpass/index.js
+++ examples/glb3/mesh_renderpass/index.js
@@ -2,4 +2,4 @@ import { RenderPass } from 'points';
 import vert from './vert.js';
 import frag from './frag.js';
 
-export const cube_renderpass = new RenderPass(vert, frag);
+export const mesh_renderpass = new RenderPass(vert, frag);
