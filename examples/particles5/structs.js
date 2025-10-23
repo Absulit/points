f97ca8f32ba64e02576940db6d11b1c222a3349c
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-10-23 15:11:35 -0600

    new particles 5 demo
    - based on particles 4

---
+++ examples/particles5/structs.js
@@ -0,0 +1,29 @@
+export const structs = /*wgsl*/`
+
+struct CustomFragment {
+    @builtin(position) position: vec4f,
+    @location(0) color: vec4f,
+    @location(1) uv: vec2f,
+    @location(2) ratio: vec2f,
+    @location(3) uvr: vec2f,
+    @location(4) mouse: vec2f,
+    @location(5) depth: vec4f,
+    @location(6) noise: f32,
+    @location(7) normal: vec3f,
+}
+
+struct Particle {
+    position: vec3f,
+    rotation: vec3f,
+    scale: vec3f,
+    color: vec4f,
+    angle: vec2f,
+    noise: f32,
+    delay: f32,
+    factor: f32,
+    speed: f32,
+    init: u32,
+}
+
+`;
+
