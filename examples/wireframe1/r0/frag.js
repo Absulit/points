238323a426367d6863a747b22de091d7facd7673
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-11-27 14:23:51 -0600

    color

--- examples/wireframe1/r0/frag.js
+++ examples/wireframe1/r0/frag.js
@@ -1,8 +1,8 @@
-import { fnusin } from 'points/animation';
+import { RED } from 'points/color';
 
 const frag = /*wgsl*/`
 
-${fnusin}
+${RED}
 
 
 @fragment
@@ -15,13 +15,7 @@ fn main(
     @builtin(position) position: vec4f
 ) -> @location(0) vec4f {
 
-    let cellSize = 20. + 10. * fnusin(1.);
-    let a = sin(uvr.x  * cellSize) * sin(uvr.y * cellSize);
-    let b = sin(uvr.x * uvr.y * 10. * 9.1 * .25 );
-    let c = fnusin(uvr.x * uvr.y * 10.);
-    let d = distance(a,b);
-    let f = d * uvr.x * uvr.y;
-    let finalColor:vec4f = vec4(a*d,f*c*a,f, 1.);
+    let finalColor = RED;
 
     return finalColor;
 }
