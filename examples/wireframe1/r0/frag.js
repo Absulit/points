62786768c27386e04043fbb75b7b3a5954e65e09
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-11-28 12:13:26 -0600

    Update frag.js

--- examples/wireframe1/r0/frag.js
+++ examples/wireframe1/r0/frag.js
@@ -8,6 +8,17 @@ ${WHITE}
 
 @fragment
 fn main(
+    /**
+     * position: vec4f,
+     * color: vec4f,
+     * uv: vec2f,
+     * ratio: vec2f,  // relation between params.screen.x and params.screen.y
+     * uvr: vec2f,    // uv with aspect ratio corrected
+     * mouse: vec2f,
+     * normal: vec3f,
+     * id: u32,       // mesh or instance id
+     * barycentrics: vec3f,
+     */
     in: Fragment
 ) -> @location(0) vec4f {
 
