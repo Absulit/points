6cef6b663a29d6d7d4dda1d523ff7485a4da9065
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-12-08 15:54:48 -0600

    removing if
    - general clean up

--- src/core/RenderPasses/crt/frag.js
+++ src/core/RenderPasses/crt/frag.js
@@ -13,24 +13,23 @@ ${rotateVector}
 ${texture}
 ${RED + GREEN + BLUE}
 
+const NUMCOLUMNS = 200.;
 
 @fragment
 fn main(in: FragmentIn) -> @location(0) vec4f {
 
-    let numColumns = 200. *  params.crt_scale;
-    let numRows = 200. * params.crt_scale;
-
+    let numColumns = NUMCOLUMNS *  params.crt_scale;
+    let numRows = NUMCOLUMNS * params.crt_scale;
 
     let pixelsWidth = params.screen.x / numColumns;
     let pixelsHeight = params.screen.y / numRows;
     let dx = pixelsWidth * (1. / params.screen.x);
     let dy = pixelsHeight * (1. / params.screen.y);
-    var x = 0.;
+
     // if column is pair then displace by this amount
-    if(floor( in.uvr.x / dx) % 2 == 0.){
-        x = .5;
-    }
-    let pixeleduv = vec2(dx*floor( in.uvr.x / dx), dy * floor( in.uvr.y / dy + x));
+    let x = select(0., .5, floor(in.uvr.x / dx) % 2 == 0.);
+
+    let pixeleduv = vec2(dx*floor( in.uvr.x / dx), dy * floor(in.uvr.y / dy + x));
     let pixeleduvColor = vec4(pixeleduv, 0, 1);
 
     let subuv = fract(in.uvr * vec2(numColumns, numRows) + vec2(0,x));
@@ -47,11 +46,10 @@ fn main(in: FragmentIn) -> @location(0) vec4f {
 
     let margin = vec2(.01,0.) * 4;
     let margin_v = vec2(.0, .01) * 8;
-    var offset = vec2(.33,0);
+    let offset = vec2(.33,0);
     let redSlot = RED * imageColorR * sdfRect(margin+margin_v+bottom_left + offset * 0, top_right - margin - margin_v + offset * 0, subuv); // subuv
     let greenSlot = GREEN * imageColorG * sdfRect(margin+margin_v+bottom_left + offset * 1, top_right - margin - margin_v + offset * 1, subuv);
     let blueSlot = BLUE * imageColorB * sdfRect(margin+margin_v+bottom_left + offset * 2, top_right - margin - margin_v + offset * 2, subuv);
-
 
     let finalColor = redSlot + greenSlot + blueSlot;
 
