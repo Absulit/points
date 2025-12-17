785a6c64cc5c9bc7e7a13d70940934fed1ec522e
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-12-17 13:33:11 -0600

    Update main.js

--- examples_tutorial/cdn/main.js
+++ examples_tutorial/cdn/main.js
@@ -10,12 +10,35 @@ const points = new Points('canvas');
 // create your render pass with three shaders as follow
 const renderPasses = [
     new RenderPass(/*wgsl*/`
+        /**
+         * VertexIn
+         * position: vec4f,
+         * color: vec4f,
+         * uv: vec2f,
+         * normal: vec3f,
+         * id: u32,       // mesh id
+         * vertexIndex: u32,
+         * instanceIndex: u32,
+         */
         @vertex
         fn main(in:VertexIn) -> FragmentIn {
             return defaultVertexBody(in.position, in.color, in.uv, in.normal);
         }`,
         /*wgsl*/`
         ${fnusin} // include the function
+
+        /**
+         * VertexIn
+         * position: vec4f,
+         * color: vec4f,
+         * uv: vec2f,
+         * ratio: vec2f,  // relation between params.screen.x and params.screen.y
+         * uvr: vec2f,    // uv with aspect ratio corrected
+         * mouse: vec2f,
+         * normal: vec3f,
+         * id: u32,       // mesh or instance id
+         * barycentrics: vec3f,
+         */
         @fragment
         fn main(in:FragmentIn) -> @location(0) vec4f {
             let cellSize = 20. + 10. * fnusin(1.);
@@ -30,7 +53,10 @@ const renderPasses = [
         }
         `,
         /*wgsl*/`
-
+        // ComputeIn
+        // @builtin(global_invocation_id) GID: vec3u,
+        // @builtin(workgroup_id)  in.WID: vec3u,
+        // @builtin(local_invocation_id) LID: vec3u
         @compute @workgroup_size(8,8,1)
         fn main(in:ComputeIn) {
             let time = params.time;
