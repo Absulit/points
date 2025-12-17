780ff31003ddcfaddea4e8e3342182338d9eae54
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-12-17 14:01:36 -0600

    updating bun

--- examples_tutorial/bun/main.js
+++ examples_tutorial/bun/main.js
@@ -10,47 +10,55 @@ const points = new Points('canvas');
 // create your render pass with three shaders as follow
 const renderPasses = [
     new RenderPass(/*wgsl*/`
+        /**
+         * VertexIn
+         * position: vec4f,
+         * color: vec4f,
+         * uv: vec2f,
+         * normal: vec3f,
+         * id: u32,       // mesh id
+         * vertexIndex: u32,
+         * instanceIndex: u32,
+         */
         @vertex
-        fn main(
-            @location(0) position:vec4f,
-            @location(1) color:vec4f,
-            @location(2) uv:vec2f,
-            @builtin(vertex_index) vertexIndex:u32
-        ) -> Fragment {
-
-            return defaultVertexBody(position, color, uv);
+        fn main(in:VertexIn) -> FragmentIn {
+            return defaultVertexBody(in.position, in.color, in.uv, in.normal);
         }`,
         /*wgsl*/`
         ${fnusin} // include the function
-        @fragment
-        fn main(
-            @location(0) color:vec4f,
-            @location(1) uv:vec2f,
-            @location(2) ratio:vec2f,  // relation between params.screen.x and params.screen.y
-            @location(3) uvr:vec2f,    // uv with aspect ratio corrected
-            @location(4) mouse:vec2f,
-            @builtin(position) position:vec4f
-        ) -> @location(0) vec4f {
 
+        /**
+         * VertexIn
+         * position: vec4f,
+         * color: vec4f,
+         * uv: vec2f,
+         * ratio: vec2f,  // relation between params.screen.x and params.screen.y
+         * uvr: vec2f,    // uv with aspect ratio corrected
+         * mouse: vec2f,
+         * normal: vec3f,
+         * id: u32,       // mesh or instance id
+         * barycentrics: vec3f,
+         */
+        @fragment
+        fn main(in:FragmentIn) -> @location(0) vec4f {
             let cellSize = 20. + 10. * fnusin(1.);
-            let a = sin(uvr.x  * cellSize) * sin(uvr.y * cellSize);
-            let b = sin(uvr.x * uvr.y * 10. * 9.1 * .25 );
-            let c = fnusin(uvr.x * uvr.y * 10.);
+            let a = sin(in.uvr.x  * cellSize) * sin(in.uvr.y * cellSize);
+            let b = sin(in.uvr.x * in.uvr.y * 10. * 9.1 * .25 );
+            let c = fnusin(in.uvr.x * in.uvr.y * 10.);
             let d = distance(a,b);
-            let f = d * uvr.x * uvr.y;
+            let f = d * in.uvr.x * in.uvr.y;
             let finalColor:vec4f = vec4(a*d,f*c*a,f, 1.);
 
             return finalColor;
         }
         `,
         /*wgsl*/`
-
+        // ComputeIn
+        // @builtin(global_invocation_id) GID: vec3u,
+        // @builtin(workgroup_id)  in.WID: vec3u,
+        // @builtin(local_invocation_id) LID: vec3u
         @compute @workgroup_size(8,8,1)
-        fn main(
-            @builtin(global_invocation_id) GlobalId:vec3u,
-            @builtin(workgroup_id) WorkGroupID:vec3u,
-            @builtin(local_invocation_id) LocalInvocationID:vec3u
-        ) {
+        fn main(in:ComputeIn) {
             let time = params.time;
         }
         `,
