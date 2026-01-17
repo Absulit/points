fa32b7f3354e4a845211fd22740428a0b93b32a6
Sebastian Sanabria Diaz <sebastian@absulit.com>
2026-01-17 14:06:26 -0600

    Update frag.js

--- examples/html_texture_1/frag.js
+++ examples/html_texture_1/frag.js
@@ -8,8 +8,9 @@ ${layer}
 
 @fragment
 fn main(in:FragmentIn) -> @location(0) vec4f {
-    let center = vec2f(.5) * in.ratio;
-    let c = texture(image, imageSampler, in.uvr - in.mouse * in.ratio, true);
+    let dims = vec2f(textureDimensions(image, 0)) / params.screen;
+    let center = vec2f(.5) * in.ratio - (dims * .5);
+    let c = texture(image, imageSampler, in.uvr - center, true);
 
     return layer(in.color, c);
 }
