26001ea07dc5894447afadf288cddb8ce0f2fb2c
Sebastian Sanabria Diaz <sebastian@absulit.com>
2026-01-23 17:55:14 -0600

    65M threads

--- examples/compute_texture_1/compute.js
+++ examples/compute_texture_1/compute.js
@@ -5,9 +5,24 @@ const compute = /*wgsl*/`
 // @builtin(global_invocation_id) GID: vec3u,
 // @builtin(workgroup_id)  in.WID: vec3u,
 // @builtin(local_invocation_id) LID: vec3u
-@compute @workgroup_size(8,8,1)
+@compute @workgroup_size(THREADS_X, THREADS_Y, THREADS_Z)
 fn main(in: ComputeIn) {
-    textureStore(writeTexture, vec2u(0,0), vec4f(1,0,0,1));
+    // index = x + (y * numColumns) + (z * numColumns * numRows)
+
+    let x = in.WID.x * THREADS_X + in.LID.x;
+    let y = in.WID.y * THREADS_Y + in.LID.y;
+    let z = in.WID.z * THREADS_Z + in.LID.z;
+
+    let X = x;
+    let Y = y * (WORKGROUP_X * THREADS_X);
+    let Z = z * (WORKGROUP_X * THREADS_X) * (WORKGROUP_Y * THREADS_Y);
+
+    let index = i32(X + Y + Z);
+    let indexF = f32(index);
+
+    let dims = vec2f(TEXTURE_WIDTH, TEXTURE_HEIGHT);
+
+    textureStore(writeTexture, vec2u(x,y), vec4f(f32(x)/dims.x,f32(y)/dims.y,0,1));
 }
 `;
 
--- examples/compute_texture_1/index.js
+++ examples/compute_texture_1/index.js
@@ -1,20 +1,49 @@
 import vert from './vert.js';
 import compute from './compute.js';
 import frag from './frag.js';
-import Points from 'points';
+import Points, { RenderPass } from 'points';
 
 const options = {
-    val: .5,
+    val: .125 * .5 * .5 * .5 * .5 * .5,
 }
 
+
+let WORKGROUP_X = 512;
+let WORKGROUP_Y = 512;
+let WORKGROUP_Z = 1;
+
+let THREADS_X = 16;
+let THREADS_Y = 16;
+let THREADS_Z = 1;
+
+const NUMPARTICLES = WORKGROUP_X * WORKGROUP_Y * WORKGROUP_Z * THREADS_X * THREADS_Y * THREADS_Z;
+console.log('NUMPARTICLES: ', NUMPARTICLES);
+
+const TEXTURE_WIDTH = WORKGROUP_X * THREADS_X;
+const TEXTURE_HEIGHT = WORKGROUP_Y * THREADS_Y;
+
+const r0 = new RenderPass(vert, frag, compute, WORKGROUP_X, WORKGROUP_Y, WORKGROUP_Z);
+
 const base = {
-    vert,
-    compute,
-    frag,
+    renderPasses: [
+        r0
+    ],
     /**
      * @param {Points} points
      */
     init: async (points, folder) => {
+        points.setConstant('NUMPARTICLES', NUMPARTICLES, 'u32');
+        points.setConstant('WORKGROUP_X', WORKGROUP_X, 'u32');
+        points.setConstant('WORKGROUP_Y', WORKGROUP_Y, 'u32');
+        points.setConstant('WORKGROUP_Z', WORKGROUP_Z, 'u32');
+        points.setConstant('THREADS_X', THREADS_X, 'u32');
+        points.setConstant('THREADS_Y', THREADS_Y, 'u32');
+        points.setConstant('THREADS_Z', THREADS_Z, 'u32');
+
+        points.setConstant('TEXTURE_WIDTH', TEXTURE_WIDTH, 'f32');
+        points.setConstant('TEXTURE_HEIGHT', TEXTURE_HEIGHT, 'f32');
+
+
         const descriptor = {
             addressModeU: 'clamp-to-edge',
             addressModeV: 'clamp-to-edge',
@@ -24,7 +53,7 @@ const base = {
             //maxAnisotropy: 10,
         }
         points.setSampler('imageSampler', descriptor);
-        points.setBindingTexture('writeTexture', 'readTexture', null, null, [4, 4]);
+        points.setBindingTexture('writeTexture', 'readTexture', null, null, [TEXTURE_WIDTH, TEXTURE_HEIGHT]);
 
         points.setUniform('val', options.val);
         folder.add(options, 'val', 0, 1, .0001).name('Val');
