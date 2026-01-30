9f66cef6518e2b69f162b44198e620c699978365
Sebastian Sanabria Diaz <sebastian@absulit.com>
2026-01-29 19:10:27 -0600

    removing else cases
    - moved as the default values of variables

--- src/core/defaultFunctions.js
+++ src/core/defaultFunctions.js
@@ -39,11 +39,13 @@ const SCALE_MODE_HEIGHT = 8;
 fn defaultVertexBody(position: vec4f, color: vec4f, uv: vec2f, normal: vec3f) -> FragmentIn {
     var result: FragmentIn;
 
-    var ratioX = 0.;
-    var ratioY = 0.;
-
     let ratio_from_x = params.screen.x / params.screen.y;
     let ratio_from_y = params.screen.y / params.screen.x;
+
+    let scale_mode_equals_height = params.scaleMode == SCALE_MODE_HEIGHT; // else SCALE_MODE_WIDTH
+
+    var ratioX = select(1., ratio_from_x, scale_mode_equals_height);
+    var ratioY = select(ratio_from_y, 1., scale_mode_equals_height);
 
     if(params.scaleMode == SCALE_MODE_FIT){
         let x_gretear_than_y = step(params.screen.y, params.screen.x);
@@ -53,29 +55,23 @@ fn defaultVertexBody(position: vec4f, color: vec4f, uv: vec2f, normal: vec3f) ->
         let x_gretear_than_y = step(params.screen.y, params.screen.x);
         ratioX = mix(ratio_from_x, 1., x_gretear_than_y);
         ratioY = mix(1., ratio_from_y, x_gretear_than_y);
-    }else{
-        let scale_mode_equals_height = params.scaleMode == SCALE_MODE_HEIGHT; // else SCALE_MODE_WIDTH
-        ratioX = select(1., ratio_from_x, scale_mode_equals_height);
-        ratioY = select(ratio_from_y, 1., scale_mode_equals_height);
     }
 
     result.ratio = vec2(ratioX, ratioY);
     result.position = position;
     result.color = color;
     result.uv = uv;
-    result.uvr = vec2();
 
     let fits_to_height = vec2(uv.x * result.ratio.x, uv.y); // (cuts width)
     let fits_to_width = vec2(uv.x, uv.y * result.ratio.y); // (cuts height)
+    result.uvr = select(fits_to_width, fits_to_height, scale_mode_equals_height);
+
     if(params.scaleMode == SCALE_MODE_FIT){
         let x_gretear_than_y = step(params.screen.y, params.screen.x);
         result.uvr = mix(fits_to_width, fits_to_height, x_gretear_than_y);
     }else if(params.scaleMode == SCALE_MODE_COVER){
         let y_gretear_than_x = step(params.screen.x, params.screen.y);
         result.uvr = mix(fits_to_width, fits_to_height, y_gretear_than_x);
-    }else{
-        let scale_mode_equals_height = params.scaleMode == SCALE_MODE_HEIGHT; // else SCALE_MODE_WIDTH
-        result.uvr = select(fits_to_width, fits_to_height, scale_mode_equals_height);
     }
 
     result.mouse = vec2(params.mouse.x / params.screen.x, params.mouse.y / params.screen.y);
