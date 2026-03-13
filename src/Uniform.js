ca2516af3a969a583d93fea628e1c680b474f4ec
Sebastian Sanabria Diaz <sebastian@absulit.com>
2026-03-12 18:36:25 -0600

    Fix to wrong validation

--- src/Uniform.js
+++ src/Uniform.js
@@ -15,7 +15,7 @@ export default class Uniform {
     constructor({ name, value, type = null, size = null }) {
 
         this.#validateName(name);
-        this.#validateType(value);
+        this.#validateType(type);
         this.#validateValue(value);
 
         this.#name = name;
