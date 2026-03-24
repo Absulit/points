f5cb8f72295ca6124d8ef3bf7a8dcd12d704bc39
Sebastian Sanabria Diaz <sebastian@absulit.com>
2026-03-24 15:43:12 -0600

    more getWGSLType validations

--- tests/main.js
+++ tests/main.js
@@ -511,5 +511,24 @@ QUnit.module('getWGSLType', hooks => {
         assert.equal(type, 'i32', 'Type should be i32');
     })
 
+    QUnit.test('If value is a string it returns empty string', assert => {
+        const type = getWGSLType('10');
+        assert.equal(type, '', 'Type should be empy string');
+    })
+
+    QUnit.test('If value is an object it returns empty string', assert => {
+        const type = getWGSLType({});
+        assert.equal(type, '', 'Type should be empy string');
+    })
+
+    QUnit.test('If value is null it returns empty string', assert => {
+        const type = getWGSLType(null);
+        assert.equal(type, '', 'Type should be empy string');
+    })
+
+    QUnit.test('If value is  0 it returns u32', assert => {
+        const type = getWGSLType(0);
+        assert.equal(type, 'u32', 'Type should be u32');
+    })
 
 })
