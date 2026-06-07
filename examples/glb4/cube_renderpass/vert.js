c134e966393ad95e45b5f022b879703f5b40b5df
Sebastian Sanabria Diaz <sebastian@absulit.com>
2026-06-06 20:02:48 -0600

    cleaning code

--- examples/glb4/cube_renderpass/vert.js
+++ examples/glb4/cube_renderpass/vert.js
@@ -14,18 +14,29 @@ fn main(in: VertexIn) -> FragmentIn {
     let joint = vec4u(joints[in.vertexIndex]);
     let weight = weights[in.vertexIndex];
 
-let skinMatrix =
-    weight.x * boneMatrices[joint.x] +
-    weight.y * boneMatrices[joint.y] +
-    weight.z * boneMatrices[joint.z] +
-    weight.w * boneMatrices[joint.w];
+    let skinMatrix =
+        weight.x * boneMatrices[joint.x] +
+        weight.y * boneMatrices[joint.y] +
+        weight.z * boneMatrices[joint.z] +
+        weight.w * boneMatrices[joint.w];
 
-    let skinnedPosition = skinMatrix * vec4f(in.position.xyz, 1);
-    let skinnedNormal   = mat3x3f(skinMatrix[0].xyz, skinMatrix[1].xyz, skinMatrix[2].xyz) * in.normal;
+    // var angleZ = params.time * 0.9854;
+    var angleY = params.time * 0.94222;
+    // var angleX = params.time * 0.865;
 
-    let clip = camera.camera_projection * camera.camera_view * skinnedPosition;
+    let rotX = rotXAxis(0);
+    let rotY = rotYAxis(angleY);
+    let rotZ = rotZAxis(0);
+    let model = rotX * rotY * rotZ;
 
-    let newNormal = normalize(in.normal);
+
+    // let skinnedPosition = skinMatrix * vec4f(in.position.xyz, 1);
+    // let skinnedNormal   = (skinMatrix * vec4f(in.normal, 0.0)).xyz;
+
+    let world = (model * skinMatrix * vec4f(in.position.xyz, 1)).xyz;
+    let clip = camera.camera_projection * camera.camera_view * vec4f(world, 1.);
+
+    let newNormal = normalize((model * skinMatrix * vec4f(in.normal, 0.)).xyz);
 
     var dvb = defaultVertexBody(clip, in.color, in.uv, newNormal);
     dvb.id = in.id;
