4fb9a471c61fd6a6bcff5a0460ea32077bd51122
Sebastian Sanabria Diaz <sebastian@absulit.com>
2026-07-15 09:29:02 -0600

    listing and creating json
    - list examples directory
    - basic json creation

--- scripts/shader_projects.sh
+++ scripts/shader_projects.sh
@@ -3,12 +3,23 @@
 json_items=""
 comma=""
 
-for name in "item1" "item2" "item2"; do
-    printf -v item '{\n    "name": "%s",\n    "status": "active"\n  }' "$name"
-    json_items+="${comma}${item}"
-    comma=",\n  "
+# create list with examples forlders to parse each and get the data
+dirlist=(`ls -d -1 examples/**/`)
+
+# echo ${dirlist[@]}
+
+for path in ${dirlist[@]}; do
+    path=".${path/examples/""}index.js"
+
+    # the check is to discard index_files directory, where the shader_projects.js is at
+    if [ -e "./examples/$path" ]; then
+        printf -v item '{\n    "path": "%s",\n    "status": "active"\n  }' "$path"
+        json_items+="${comma}${item}"
+        comma=",\n  "
+    fi
+
 done
 
 printf -v json_output '[\n  %s\n]' "$json_items"
 
-echo -e "$json_output"
+echo -e "$json_output" > "test.json"
