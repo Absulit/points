56a5aaf3d156b27cf31b15d1bdaa45add8471fcd
Sebastian Sanabria Diaz <sebastian@absulit.com>
2026-07-15 11:25:13 -0600

    output info data to master json

--- scripts/shader_projects.sh
+++ scripts/shader_projects.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 json_items=""
 comma=""
@@ -9,13 +9,33 @@ dirlist=(`ls -d -1 examples/**/`)
 # echo ${dirlist[@]}
 
 for path in ${dirlist[@]}; do
+    info="${path}info"
     path=".${path/examples/""}index.js"
 
+
     # the check is to discard index_files directory, where the shader_projects.js is at
-    if [ -e "./examples/$path" ]; then
-        printf -v item '{\n    "path": "%s",\n    "status": "active"\n  }' "$path"
+    if [[ -e "./examples/$path" && -e $info ]]; then
+        source $info
+        echo $desc
+
+        printf -v item '{
+            "name":"%s",
+            "path":"%s",
+            "uri":"%s",
+            "desc":"%s",
+            "author":"%s",
+            "authlink":"%s",
+            "fitwindow":"%s",
+            "enabled":"%s",
+            "tax":"%s"
+        }' "$name" "$path" "$uri" "$desc" "$author" "$authlink" "$fitwindow" "$enabled" "$tax"
+
+        # remove end of lines and extra spaces
+        item="${item//$'\n'/}"
+        item="${item//  /}"
+
         json_items+="${comma}${item}"
-        comma=",\n  "
+        comma=",  "
     fi
 
 done
